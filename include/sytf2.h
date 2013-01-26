0d79c2e512a749863eebaca4e6d5d772efc85005
Rodney James <rodney.james@ucdenver.edu>
2013-01-26 06:36:43 -0700

    fixed a couple of problems in complex version of sytf2

--- include/sytf2.h
+++ include/sytf2.h
@@ -16,7 +16,17 @@
 #include "scal.h"
 #include "swap.h"
 #include <cmath>
+#include <algorithm>
 #include "latl.h"
+
+template<typename real_t>
+static real_t abs1(complex<real_t> z)
+{
+   using std::abs;
+   using std::real;
+   using std::imag;
+   return abs(real(z))+abs(imag(z));
+}
 
 namespace latl
 {
@@ -24,23 +34,34 @@ namespace latl
    ///
    ///         A = U*D*U' if uplo = 'U'
    ///         A = L'*D*L if uplo = 'L'
-   ///
-   /// where U (or L) is a product of permutation and unit upper (lower) triangular matrices, U' is the transpose of U, and D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
-   ///
+   /// where U (or L) is a product of permutation and unit upper (lower) triangular matrices,
+   /// U' is the transpose of U, and D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
    /// @return 0 if success
    /// @return -i if the ith argument is invalid.
-   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is exactly singular and division by zero will occur if it is used to solve a system of equations.
+   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is
+   /// exactly singular and division by zero will occur if it is used to solve a system of equations.
    /// @tparam real_t Floating point type.
-   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.
+   /// The other triangular part of A is not referenced.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the multipliers used to obtain the factor U or L.
+   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.
+   /// On exit, the block diagonal matrix D and the multipliers used to obtain the factor U or L.
    /// @param ldA Column length of the array A.  ldA >= n
    /// @param IPIV Integer array size n.  On exit, contains the details of the interchanges of D.
-   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
+   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.
+   /// If BSDV[k] = 0, then rows and columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.
+   /// If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1],
+   /// then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1],
+   /// then rows and columns k+1 and IPIV[k] were interchanged.
    
    template<typename real_t>
    int_t sytf2(const char uplo, const int_t n, real_t * const A, const int_t ldA, int_t * IPIV, bool * BSDV)
    {
+      using std::abs;
+      using std::isnan;
+      using std::sqrt;
+      using std::max;
+      
       if (uplo != 'U' && uplo != 'u' && uplo != 'L' && uplo != 'l')
          return -1;
       if ( n < 0)
@@ -51,7 +72,7 @@ namespace latl
       if (n == 0)
          return 0;
       
-      const real_t alpha = (1.0 + std::sqrt(17.0))/8.0;
+      const real_t alpha = (1.0 + sqrt(17.0))/8.0;
       const real_t one(1.0);
       const real_t zero(0.0);
       real_t * Ak, absakk, colmax, * Akm1;
@@ -71,19 +92,19 @@ namespace latl
             Ak = A + ldA*k;
             Akm1 = Ak-ldA;
             
-            absakk = std::abs(Ak[k]);
+            absakk = abs(Ak[k]);
             
             if (k > 0)
             {
                imax = latl::imax(k, Ak, 1);
-               colmax = std::abs(Ak[imax]);
+               colmax = abs(Ak[imax]);
             }
             else
             {
                colmax = zero;
             }
             
-            if (std::max(absakk, colmax) == zero || std::isnan(absakk))
+            if (max(absakk, colmax) == zero || isnan(absakk))
             {
                if (info == 0)
                   info = k+1;
@@ -103,16 +124,16 @@ namespace latl
                   jmax = imax+1+ latl::imax(k-imax, A+ldA*(imax+1)+imax, ldA);
                   real_t * Ajmax = A + ldA*jmax;
                   real_t * Aimax = A + ldA*imax;
-                  rowmax = std::abs(Ajmax[imax]);
+                  rowmax = abs(Ajmax[imax]);
                   if (imax > 0)
                   {
                      jmax = latl::imax(imax, Aimax, 1);
-                     rowmax = std::max(rowmax, std::abs(Aimax[jmax]));
+                     rowmax = max(rowmax, abs(Aimax[jmax]));
                   }
                   
                   if (absakk >= alpha*colmax*(colmax/rowmax))
                      kp = k;
-                  else if (std::abs(Aimax[imax]) >= alpha*rowmax)
+                  else if (abs(Aimax[imax]) >= alpha*rowmax)
                      kp = imax;
                   else
                   {
@@ -196,19 +217,19 @@ namespace latl
             kstep = 1;
             Ak = A + ldA*k;
             
-            absakk = std::abs(Ak[k]);
+            absakk = abs(Ak[k]);
             
             if (k < n-1)
             {
                imax = k +1 + latl::imax(n-k-1, Ak+k+1, 1);
-               colmax = std::abs(Ak[imax]);
+               colmax = abs(Ak[imax]);
             }
             else
             {
                colmax = zero;
             }
             
-            if (std::max(absakk, colmax) == zero || std::isnan(absakk))
+            if (max(absakk, colmax) == zero || isnan(absakk))
             {
                if (info == 0)
                   info = k;
@@ -223,16 +244,16 @@ namespace latl
                   int_t jmax = k + latl::imax(imax-k, Ak+imax, ldA);
                   real_t * Ajmax = A + ldA*jmax;
                   real_t *Aimax = A +ldA*imax;
-                  real_t rowmax = std::abs(Ajmax[imax]);
+                  real_t rowmax = abs(Ajmax[imax]);
                   if (imax < n-1)
                   {
                      jmax = imax +latl::imax(n-imax-1, Aimax+imax+1, 1);
-                     rowmax = std::max(rowmax, std::abs(Aimax[jmax]));
+                     rowmax = max(rowmax, abs(Aimax[jmax]));
                   }
                   
                   if (absakk >= alpha*colmax*(colmax/rowmax))
                      kp = k;
-                  else if (std::abs(Aimax[imax]) >= alpha*rowmax)
+                  else if (abs(Aimax[imax]) >= alpha*rowmax)
                   {
                      kp = imax;
                   }
@@ -318,23 +339,33 @@ namespace latl
    ///
    ///         A = U*D*U' if uplo = 'U'
    ///         A = L'*D*L if uplo = 'L'
-   ///
-   /// where U (or L) is a product of permutation and unit upper (lower) triangular matrices, U' is the transpose of U, and D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
-   ///
+   /// where U (or L) is a product of permutation and unit upper (lower) triangular matrices, U' is the transpose of U, and
+   /// D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
    /// @return 0 if success
    /// @return -i if the ith argument is invalid.
-   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is exactly singular and division by zero will occur if it is used to solve a system of equations.
+   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is
+   /// exactly singular and division by zero will occur if it is used to solve a system of equations.
    /// @tparam real_t Floating point type.
-   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.
+   /// The other triangular part of A is not referenced.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Complex array size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the multipliers used to obtain the factor U or L.
+   /// @param A Complex array size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the
+   /// multipliers used to obtain the factor U or L.
    /// @param ldA Column length of the array A.  ldA >= n
    /// @param IPIV Integer array size n.  On exit, contains the details of the interchanges of D.
-   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
+   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and
+   /// columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2
+   /// diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were
+   /// interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
    
    template<typename real_t>
    int_t sytf2(const char uplo, const int_t n, complex<real_t> * const A, const int_t ldA, int_t * IPIV, bool * BSDV)
    {
+      using std::abs;
+      using std::isnan;
+      using std::sqrt;
+      using std::max;
+      
       if (uplo != 'U' && uplo != 'u' && uplo != 'L' && uplo != 'l')
          return -1;
       if ( n < 0)
@@ -345,11 +376,10 @@ namespace latl
       if (n == 0)
          return 0;
       
-      const real_t alpha = (1.0 + std::sqrt(17.0))/8.0;
-      const real_t one(1.0);
-      const complex<real_t> onec(1.0);
+      const real_t alpha = (1.0 + sqrt(17.0))/8.0;
+      const complex<real_t> one(1.0);
       const real_t zero(0.0);
-      complex<real_t> * Ak,* Akm1, d11, d22;
+      complex<real_t> * Ak, * Akm1;
       real_t absakk, colmax;
       int_t info = 0, imax = 0, kp;
       
@@ -367,19 +397,19 @@ namespace latl
             Ak = A + ldA*k;
             Akm1 = Ak-ldA;
             
-            absakk = std::abs(Ak[k]);
+            absakk = abs1(Ak[k]);
             
             if (k > 0)
             {
                imax = latl::imax(k, Ak, 1);
-               colmax = std::abs(Ak[imax]);
+               colmax = abs1(Ak[imax]);
             }
             else
             {
                colmax = zero;
             }
             
-            if (std::max(absakk, colmax) == zero || std::isnan(absakk))
+            if (max(absakk, colmax) == zero || isnan(absakk))
             {
                if (info == 0)
                   info = k+1;
@@ -399,16 +429,16 @@ namespace latl
                   jmax = imax+1+ latl::imax(k-imax, A+ldA*(imax+1)+imax, ldA);
                   complex<real_t> * Ajmax = A + ldA*jmax;
                   complex<real_t> * Aimax = A + ldA*imax;
-                  rowmax = std::abs(Ajmax[imax]);
+                  rowmax = abs1(Ajmax[imax]);
                   if (imax > 0)
                   {
                      jmax = latl::imax(imax, Aimax, 1);
-                     rowmax = std::max(rowmax, std::abs(Aimax[jmax]));
+                     rowmax = max(rowmax, abs1(Aimax[jmax]));
                   }
                   
                   if (absakk >= alpha*colmax*(colmax/rowmax))
                      kp = k;
-                  else if (std::abs(Aimax[imax]) >= alpha*rowmax)
+                  else if (abs1(Aimax[imax]) >= alpha*rowmax)
                      kp = imax;
                   else
                   {
@@ -439,19 +469,19 @@ namespace latl
                
                if (kstep == 1)
                {
-                  r1 = onec/Ak[k];
+                  r1 = one/Ak[k];
                   latl::syr(uplo, k, -r1, Ak, 1, A, ldA);
-                  latl::scal(k, r1, Ak, 1);
+                  latl::scal<real_t>(k, real(r1), Ak, 1);
                }
                else
                {
                   if (k > 1)
                   {
-                     complex<real_t> d12;
+                     complex<real_t> d12, d22, d11;
                      d12 = Ak[k-1];
                      d22 = Akm1[k-1]/d12;
                      d11 = Ak[k]/d12;
-                     d12 = (onec/(d11*d22-onec))/d12;
+                     d12 = (one/(d11*d22-one))/d12;
                      
                      complex<real_t> wk, wkm1;
                      for (int_t j = k-2; j >= 0; --j)
@@ -492,19 +522,19 @@ namespace latl
             kstep = 1;
             Ak = A + ldA*k;
             
-            absakk = std::abs(Ak[k]);
+            absakk = abs1(Ak[k]);
             
             if (k < n-1)
             {
                imax = k +1 + latl::imax(n-k-1, Ak+k+1, 1);
-               colmax = std::abs(Ak[imax]);
+               colmax = abs1(Ak[imax]);
             }
             else
             {
                colmax = zero;
             }
             
-            if (std::max(absakk, colmax) == zero || std::isnan(absakk))
+            if (max(absakk, colmax) == zero || isnan(absakk))
             {
                if (info == 0)
                   info = k;
@@ -519,16 +549,16 @@ namespace latl
                   int_t jmax = k + latl::imax(imax-k, Ak+imax, ldA);
                   complex<real_t> * Ajmax = A + ldA*jmax;
                   complex<real_t> *Aimax = A +ldA*imax;
-                  real_t rowmax = std::abs(Ajmax[imax]);
+                  real_t rowmax = abs1(Ajmax[imax]);
                   if (imax < n-1)
                   {
                      jmax = imax +latl::imax(n-imax-1, Aimax+imax+1, 1);
-                     rowmax = std::max(rowmax, std::abs(Aimax[jmax]));
+                     rowmax = max(rowmax, abs(Aimax[jmax]));
                   }
                   
                   if (absakk >= alpha*colmax*(colmax/rowmax))
                      kp = k;
-                  else if (std::abs(Aimax[imax]) >= alpha*rowmax)
+                  else if (abs1(Aimax[imax]) >= alpha*rowmax)
                   {
                      kp = imax;
                   }
@@ -561,16 +591,16 @@ namespace latl
                {
                   if (k < n-1)
                   {
-                     complex<real_t> r1 = onec/Ak[k];
+                     complex<real_t> r1 = one/Ak[k];
                      latl::syr(uplo, n-k-1, -r1, Ak+k+1, 1, Ak+ldA+k+1, ldA);
-                     latl::scal(n-k-1, r1, Ak+k+1, 1);
+                     latl::scal<real_t>(n-k-1, real(r1), Ak+k+1, 1);
                   }
                }
                else
                {
                   if (k < n-2)
                   {
-                     complex<real_t> d21, temp, wk, wkp1;
+                     complex<real_t> d21, d11, d22, temp, wk, wkp1;
                      complex<real_t> * Akp1 = A+ldA*(k+1);
                      d21 = Ak[k+1];
                      d11 = Akp1[k+1]/d21;
--- include/sytrf.h
+++ include/sytrf.h
@@ -17,24 +17,30 @@
 
 namespace latl
 {
-   /// @brief Computes the factorization of a real symmetric matrix A using the Bunch-Kaufman diagonal pivoting method.  The form of the factorization is
+   /// @brief Computes the factorization of a real symmetric matrix A using the Bunch-Kaufman diagonal pivoting method.
+   ///
+   /// The form of the factorization is
    ///
    ///         A = U*D*U' if uplo = 'U'
    ///         A = L'*D*L if uplo = 'L'
-   ///
-   /// where U (or L) is a product of permutation and unit upper (or lower) triangular matrices and D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
-   ///
-   /// This is the blocked version of the algorithm.
+   /// where U (or L) is a product of permutation and unit upper (or lower) triangular matrices and D is symmetric and
+   /// block diagonal with 1-by-1 and 2-by-2 diagonal blocks.  This is the blocked version of the algorithm.
    /// @return 0 if success
    /// @return -i if the ith argument is invalid.
-   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is exactly singular and division by zero will occur if it is used to solve a system of equations.
+   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D
+   /// is exactly singular and division by zero will occur if it is used to solve a system of equations.
    /// @tparam real_t Floating point type.
-   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other
+   /// triangular part of A is not referenced.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the multipliers used to obtain the factor U or L.
+   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D
+   /// and the multipliers used to obtain the factor U or L.
    /// @param ldA Column length of the array A.
    /// @param IPIV Integer array size n.  On exit, contains the details of the interchanges of D.
-   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
+   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and
+   /// columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2
+   /// diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.
+   /// If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
    /// @param nb Block size, optional.  Default value is 64.
    /// @ingroup TRF
    
@@ -109,24 +115,30 @@ namespace latl
       return info;
    }
    
-   /// @brief Computes the factorization of a complex symmetric matrix A using the Bunch-Kaufman diagonal pivoting method.  The form of the factorization is
+   /// @brief Computes the factorization of a complex symmetric matrix A using the Bunch-Kaufman diagonal pivoting method.
+   ///
+   /// The form of the factorization is
    ///
    ///         A = U*D*U' if uplo = 'U'
    ///         A = L'*D*L if uplo = 'L'
-   ///
-   /// where U (or L) is a product of permutation and unit upper (or lower) triangular matrices and D is symmetric and block diagonal with 1-by-1 and 2-by-2 diagonal blocks.
-   ///
-   /// This is the blocked version of the algorithm.
+   /// where U (or L) is a product of permutation and unit upper (or lower) triangular matrices and D is symmetric and block diagonal
+   /// with 1-by-1 and 2-by-2 diagonal blocks.  This is the blocked version of the algorithm.
    /// @return 0 if success
    /// @return -i if the ith argument is invalid.
-   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is exactly singular and division by zero will occur if it is used to solve a system of equations.
+   /// @return i+1 if the ith pivot is exactly zero.  The factorization has been completed, but the block diagonal matrix D is exactly
+   /// singular and division by zero will occur if it is used to solve a system of equations.
    /// @tparam real_t Floating point type.
-   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part
+   /// of A is not referenced.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the multipliers used to obtain the factor U or L.
+   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the symmetric matrix A.  On exit, the block diagonal matrix D and the
+   /// multipliers used to obtain the factor U or L.
    /// @param ldA Column length of the array A.
    /// @param IPIV Integer array size n.  On exit, contains the details of the interchanges of D.
-   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and columns k and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.  In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
+   /// @param BSDV Bool array size n.  On exit, contains the details of the block structure of D.  If BSDV[k] = 0, then rows and columns k
+   /// and IPIV[k] were interchanged and D[k, k] is a 1-by-1 diagonal block.  If BSDV[k] = 1, then k is part of a 2-by-2 diagonal block.
+   /// In a 2 by 2 block, if uplo = 'U', and IPIV[k] = IPIV[k-1], then rows and columns k-1 and IPIV[k] were interchanged.  If uplo = 'L' and
+   /// IPIV[k] = IPIV[k+1], then rows and columns k+1 and IPIV[k] were interchanged.
    /// @param nb Block size, optional.  Default value is 64.
    /// @ingroup TRF
    
