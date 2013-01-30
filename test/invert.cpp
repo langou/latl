d3853318f69ebea5e92112e271449fe845cb0433
Rodney James <rodney.james@ucdenver.edu>
2013-01-29 20:14:02 -0700

    added more features to invert.cpp matrix inverse test program

--- test/invert.cpp
+++ test/invert.cpp
@@ -5,7 +5,9 @@
 //  Created by Rodney James on 1/25/13.
 //  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
 //
-//  Computes inverse of a matrix and its residual
+//  Computes inverse of a matrix and reports its residual error:
+//
+//  max{ || A * A^(-1) - I ||,  || A^(-1) * A - I || } / (cond(A) * epsilon * n)
 //
 
 #include <iostream>
@@ -18,6 +20,7 @@
 #include <lantr.h>
 #include <limits>
 #include <cstdlib>
+#include <cstring>
 
 using namespace latl;
 using namespace std;
@@ -28,7 +31,7 @@ using namespace std;
 
 typedef long double ldouble;
 
-// triangular matrix
+// triangular matrix inverse test
 
 template <typename real_t,typename matrix_t> real_t triangular(char uplo, char diag)
 {
@@ -50,41 +53,94 @@ template <typename real_t,typename matrix_t> real_t triangular(char uplo, char d
    matrix_t *B=new matrix_t[n*n];
    matrix_t *C=new matrix_t[n*n];
    matrix_t *D=new matrix_t[n*n];
-   lacpy<real_t>('A',n,n,A,n,B,n);
-   if(diag=='u')
-      for(int i=0;i<n;i++)
-         B[i+i*n]=one;
+   lacpy<real_t>(uplo,n,n,A,n,B,n);
    int info=trti2<real_t>(uplo,diag,n,A,n);
    if(info>0)
    {
       cerr << "input matrix is singular" << endl;
       exit(0);
    }
-   else
-   {
-      real_t cond=lantr<real_t>('i',uplo,diag,n,n,A,n)*lantr<real_t>('i',uplo,diag,n,n,B,n);
-      laset<real_t>(uplo,n,n,zero,one,C,n);
-      laset<real_t>(uplo,n,n,zero,one,D,n);
-      ttmm<real_t>(uplo,n,one,A,n,B,n,-one,C,n);
-      ttmm<real_t>(uplo,n,one,B,n,A,n,-one,D,n);
-      residual=max(lantr<real_t>('m',uplo,diag,n,n,C,n),lantr<real_t>('m',uplo,diag,n,n,D,n));
-      real_t eps=numeric_limits<real_t>::epsilon();
-      cout << "condition = " << cond << endl;
-      cout << "epsilon   = " << eps << endl;
-      cout << "residual  = " << residual << endl;
-   }
+   real_t cond=lantr<real_t>('i',uplo,diag,n,n,A,n)*lantr<real_t>('i',uplo,diag,n,n,B,n);
+   laset<real_t>(uplo,n,n,zero,one,C,n);
+   laset<real_t>(uplo,n,n,zero,one,D,n);
+   if(diag=='u')
+      for(int i=0;i<n;i++)
+         A[i+i*n]=B[i+i*n]=one;
+   ttmm<real_t>(uplo,n,one,A,n,B,n,-one,C,n);
+   ttmm<real_t>(uplo,n,one,B,n,A,n,-one,D,n);
+   residual=max(lantr<real_t>('m',uplo,'n',n,n,C,n),lantr<real_t>('m',uplo,'n',n,n,D,n));
+   real_t eps=numeric_limits<real_t>::epsilon();
+//   cout << "condition = " << cond << endl;
+//   cout << "epsilon   = " << eps << endl;
+//   cout << "residual  = " << residual << endl;
    delete [] D;
    delete [] C;
    delete [] B;
-   return residual;
+   delete [] A;
+   return residual/(n*cond*eps);
+}
+
+void usage(char *name)
+{
+   cerr << "Usage: " << name << " -t <uplo> [-c] [-u]" << endl;
+   cerr << endl;
+   cerr << "           -c : use complex (default is to use real)" << endl;
+   cerr << "           -t <uplo> : invert triangular matrix where uplo is either U or L" << endl;
+   cerr << "           -u : assume unit triangular matrix" << endl;
 }
 
 int main(int argc,char **argv)
 {
-   typedef REAL real_t;
-   char uplo='L';
-   char diag='N';
-//   triangular<REAL,REAL>(uplo,diag);
-   triangular<REAL,complex<REAL> >(uplo,diag);
+   typedef REAL Real;
+   typedef complex<REAL> Complex;
+
+   int arg=1;
+   char uplo;
+   char diag='n';
+   char Type='_';
+   bool UseComplex=0;
+
+   while(arg<argc)
+   {
+      if(strncmp(argv[arg],"-t",2)==0)
+      {
+         Type='T';
+         arg++;
+         uplo=tolower(*argv[arg]);
+         if((uplo!='l')&&(uplo!='u'))
+         {
+            cerr << "uplo must be either U for upper or L for lower" << endl;
+            return 1;
+         }
+      }
+      else if(strncmp(argv[arg],"-c",2)==0)
+      {
+         UseComplex=1;
+      }
+      else if(strncmp(argv[arg],"-u",2)==0)
+      {
+         diag='u';
+      }
+      else
+      {
+         usage(argv[0]);
+         return 1;
+      }
+      arg++;
+   }
+
+   Real error;
+
+   if(Type=='T')
+   {
+      if(UseComplex)
+         error=triangular<Real,Complex>(uplo,diag);
+      else
+         error=triangular<Real,Real>(uplo,diag);
+      cout << error << endl;
+      return 0;
+   }
+
+   usage(argv[0]);
    return 0;
 }
