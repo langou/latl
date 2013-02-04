12fa56589b9b9e74202d210516fc434fb6f32604
Rodney James <rodney.james@ucdenver.edu>
2013-02-04 13:13:39 -0700

    added symmetric test to invert

--- test/invert.cpp
+++ test/invert.cpp
@@ -11,11 +11,12 @@
 //
 
 #include <iostream>
-#include <trtri.h>
 #include <print.h>
 #include <load.h>
 #include <ttmm.h>
 #include <gemm.h>
+#include <symm.h>
+#include <hemm.h>
 #include <lange.h>
 #include <lacpy.h>
 #include <laset.h>
@@ -23,8 +24,15 @@
 #include <limits>
 #include <cstdlib>
 #include <cstring>
+#include <trtri.h>
 #include <getrf.h>
 #include <getri.h>
+#include <sytrf.h>
+#include <sytri.h>
+#include <potrf.h>
+#include <potri.h>
+#include <hetrf.h>
+#include <hetri.h>
 
 using namespace latl;
 using namespace std;
@@ -37,7 +45,8 @@ typedef long double ldouble;
 
 // general matrix inverse test
 
-template <typename real_t,typename matrix_t> real_t general(bool resi,bool prnt)
+template <typename real_t,typename matrix_t>
+real_t general(bool resi,bool prnt)
 {
    char uplo='a';
    int m,n;
@@ -45,14 +54,9 @@ template <typename real_t,typename matrix_t> real_t general(bool resi,bool prnt)
    const matrix_t zero(0.0);
    real_t residual;
    matrix_t *A=load<matrix_t>(m,n);
-   if(A==NULL)
+   if((A==NULL)||(m!=n))
    {
       cerr << "error reading input matrix" << endl;
-      exit(0);
-   }
-   if(m!=n)
-   {
-      cerr << "input matrix is not square" << endl;
       exit(0);
    }
    matrix_t *B=new matrix_t[n*n];
@@ -84,23 +88,65 @@ template <typename real_t,typename matrix_t> real_t general(bool resi,bool prnt)
    return (resi)? residual : residual/(n*condition*eps);
 }
 
-// triangular matrix inverse test
+// symmetric matrix inverse test
 
-template <typename real_t,typename matrix_t> real_t triangular(char uplo, char diag,bool resi,bool prnt)
+template <typename real_t,typename matrix_t>
+real_t symmetric(char uplo,bool resi,bool prnt)
 {
    int m,n;
    const matrix_t one(1.0);
    const matrix_t zero(0.0);
    real_t residual;
    matrix_t *A=load<matrix_t>(m,n);
-   if(A==NULL)
+   if((A==NULL)||(m!=n))
    {
       cerr << "error reading input matrix" << endl;
       exit(0);
    }
-   if(m!=n)
+   matrix_t *B=new matrix_t[n*n];
+   matrix_t *C=new matrix_t[n*n];
+   matrix_t *D=new matrix_t[n*n];
+   int_t *ipiv=new int_t[n];
+   bool *block=new bool[n];
+   lacpy<real_t>(uplo,n,n,A,n,B,n);
+   int info=sytrf<real_t>(uplo,n,A,n,ipiv,block);
+   info=sytri<real_t>(uplo,n,A,n,ipiv,block);
+   if(info>0)
    {
-      cerr << "input matrix is not square" << endl;
+      cerr << "input matrix is singular" << endl;
+      exit(0);
+   }
+   if(prnt)
+      print<real_t>(uplo,n,A,n);
+   laset<real_t>('a',n,n,zero,one,C,n);
+   laset<real_t>('a',n,n,zero,one,D,n);
+   symm<real_t>('l','n',n,n,one,A,n,B,n,-one,C,n);
+   symm<real_t>('r','n',n,n,one,B,n,A,n,-one,D,n);
+   residual=max(lange<real_t>('m',n,n,C,n),lange<real_t>('m',n,n,D,n));
+   real_t condition=lange<real_t>('i',n,n,A,n)*lange<real_t>('i',n,n,B,n);
+   real_t eps=numeric_limits<real_t>::epsilon();
+   delete [] block;
+   delete [] ipiv;
+   delete [] D;
+   delete [] C;
+   delete [] B;
+   delete [] A;
+   return (resi)? residual : residual/(n*condition*eps);
+}
+
+// triangular matrix inverse test
+
+template <typename real_t,typename matrix_t>
+real_t triangular(char uplo, char diag,bool resi,bool prnt)
+{
+   int m,n;
+   const matrix_t one(1.0);
+   const matrix_t zero(0.0);
+   real_t residual;
+   matrix_t *A=load<matrix_t>(m,n);
+   if((A==NULL)||(m!=n))
+   {
+      cerr << "error reading input matrix" << endl;
       exit(0);
    }
    matrix_t *B=new matrix_t[n*n];
@@ -134,15 +180,18 @@ template <typename real_t,typename matrix_t> real_t triangular(char uplo, char d
 
 void usage(char *name)
 {
-   cerr << "Usage: " << name << " [-complex] [-triangular | -general] [-lower] [-unit] [-print] [-residual]" << endl;
-   cerr << endl;
-   cerr << "           -complex uses complex (default is to use real)" << endl;
-   cerr << "           -triangular inverts upper or lower triangular matrix" << endl;
-   cerr << "           -general inverts general matrix" << endl;
-   cerr << "           -lower uses lower triangular matrix (default is upper triangular)" << endl;
-   cerr << "           -unit assumes unit triangular matrix" << endl;
-   cerr << "           -print prints inverse matrix to standard output" << endl;
-   cerr << "           -residual reports the residual error instead of the relative error" << endl;
+   cerr << "Usage: " << name << " [-general | -triangular | -symmetric | -hermitian | -positive]";
+   cerr << " [-complex] [-lower] [-unit] [-print] [-residual]" << endl;
+   cerr << "           -general      invert general matrix (default)" << endl;
+   cerr << "           -triangular   invert upper or lower triangular matrix" << endl;
+   cerr << "           -symmetric    invert symmetric matrix" << endl;
+   cerr << "           -hermitian    invert hermitian matrix" << endl;
+   cerr << "           -positive     invert positive definite matrix" << endl;
+   cerr << "           -complex      use complex (default is to use real)" << endl;
+   cerr << "           -lower        use lower triangular matrix (default is upper triangular)" << endl;
+   cerr << "           -unit         assume unit triangular matrix" << endl;
+   cerr << "           -print        write inverse matrix to standard output" << endl;
+   cerr << "           -residual     report the residual error instead of the relative error" << endl;
 }
 
 int main(int argc,char **argv)
@@ -160,19 +209,25 @@ int main(int argc,char **argv)
 
    while(arg<argc)
    {
-      if(strncmp(argv[arg],"-triangular",11)==0)
+      if(strncmp(argv[arg],"-triangular",2)==0)
          Type='T';
-      else if(strncmp(argv[arg],"-general",8)==0)
+      else if(strncmp(argv[arg],"-general",2)==0)
          Type='G';
-      else if(strncmp(argv[arg],"-lower",6)==0)
+      else if(strncmp(argv[arg],"-symmetric",2)==0)
+         Type='G';
+      else if(strncmp(argv[arg],"-hermitian",2)==0)
+         Type='G';
+      else if(strncmp(argv[arg],"-positive",3)==0)
+         Type='G';
+      else if(strncmp(argv[arg],"-lower",2)==0)
          uplo='l';
-      else if(strncmp(argv[arg],"-complex",8)==0)
+      else if(strncmp(argv[arg],"-complex",2)==0)
          comp=1;
-      else if(strncmp(argv[arg],"-unit",5)==0)
+      else if(strncmp(argv[arg],"-unit",2)==0)
          diag='u';
-      else if(strncmp(argv[arg],"-print",6)==0)
+      else if(strncmp(argv[arg],"-print",3)==0)
          prnt=1;
-      else if(strncmp(argv[arg],"-residual",9)==0)
+      else if(strncmp(argv[arg],"-residual",2)==0)
          resi=1;
       else
       {
@@ -199,6 +254,15 @@ int main(int argc,char **argv)
          error=general<Real,Complex>(resi,prnt);
       else
          error=general<Real,Real>(resi,prnt);
+      cerr << error << endl;
+      return 0;
+   }
+   else if(Type=='S')
+   {
+      if(comp)
+         error=symmetric<Real,Complex>(uplo,resi,prnt);
+      else
+         error=symmetric<Real,Real>(uplo,resi,prnt);
       cerr << error << endl;
       return 0;
    }
