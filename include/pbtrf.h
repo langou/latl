4e876a5b8a320522ae2e7596f13bafc77d132fda
Rodney James <rodney.james@ucdenver.edu>
2013-02-10 18:13:40 -0700

    moved unblocked p*tf2 into block p*trf

--- include/pbtrf.h
+++ include/pbtrf.h
@@ -11,12 +11,15 @@
 
 /// @file pbtrf.h Computes the Cholesky factorization of a symmetric positive definite band matrix A.
 
-#include "pbtf2.h"
 #include "gemm.h"
 #include "herk.h"
-#include "potf2.h"
+#include "potrf.h"
 #include "syrk.h"
 #include "trsm.h"
+#include "scal.h"
+#include "syr.h"
+#include "her.h"
+#include "lacgv.h"
 #include "latl.h"
 
 namespace latl
@@ -38,11 +41,180 @@ namespace latl
    /// @param kd The number of super-diagonals of the matrix A if uplo = 'U' or the number of sub-diagonals if uplo = 'L'.  kd >= 0
    /// @param AB Real array size ldAB-by-n.  On entry, the upper or lower triangle of the symmetric band matrix A, stored in the first kd+1 rows of the array. On exit, the triangular factor U or L from the Cholesky factorization A = U' * U or A = L * L' of the band matrix A in the same storage format as A.
    /// @param ldAB Column length of the array AB.  ldAB >= kd+1;
-   /// @param nb Block size, optional.  Default value is 64.
    /// @ingroup TRF
+
+   template< typename real_t>
+   int_t pbtrf(const char uplo, const int_t n, const int_t kd, real_t * const AB, const int_t ldAB)
+   {
+      if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
+         return -1;
+      if ( n < 0)
+         return -2;
+      if (kd < 0 )
+         return -3;
+      if (ldAB < kd+1)
+         return -5;
+
+      if ( n == 0)
+         return 0;
+
+      const real_t one(1.0);
+      const real_t zero(0.0);
+      int_t kn, kld = ldAB-1;
+      real_t ajj;
+      real_t * ABj = AB;
+      real_t * ABjp1 = AB+ldAB;
+
+      if (uplo == 'U' || uplo == 'u')
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = ABj[kd];
+            if (ajj <= zero || std::isnan(ajj))
+               return j+1;
+            ABj[kd] = ajj = sqrt(ajj);
+
+            kn = std::min(kd, n-j-1);
+            if (kn > 0)
+            {
+               latl::scal(kn, one/ajj, ABjp1+kd-1, kld);
+               latl::syr('U', kn, -one, ABjp1+kd-1, kld, ABjp1+kd, kld);
+            }
+            ABj += ldAB;
+            ABjp1 += ldAB;
+         }
+      }
+      else
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = ABj[0];
+            if (ajj <= 0 || std::isnan(ajj))
+               return j+1;
+            ABj[0] = ajj = sqrt(ajj);
+            kn = std::min(kd, n-j-1);
+            if (kn > 0)
+            {
+               latl::scal(kn, one/ajj, ABj+1, 1);
+               latl::syr('L', kn, -one, ABj+1, 1, ABjp1, kld);
+            }
+            ABj += ldAB;
+            ABjp1 += ldAB;
+         }
+      }
+      return 0;
+   }
+
+   /// @brief Computes the Cholesky factorization of a complex Hermitian positive definite band matrix A.
+   ///
+   /// The factorization has the form
+   ///
+   ///         A = U^H * U   if uplo = 'U'
+   ///         A = L * L^H   if uplo = 'L'
+   ///
+   /// where U is upper triangular and U^H is the conjugate transpose of U, and L is lower triangular.
+   /// @return 0 if success
+   /// @return -i if the ith argument is invalid.
+   /// @return i+1 if the leading minor of order i is not positive definite.
+   /// @tparam real_t Floating point type.
+   /// @param uplo Indicates whether the Hermitian matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param n Order of the matrix A.  n >= 0
+   /// @param kd The number of super-diagonals of the matrix A if uplo = 'U' or the number of sub-diagonals if uplo = 'L'.  kd >= 0
+   /// @param AB Complex array size ldAB-by-n.  On entry, the upper or lower triangle of the Hermitian band matrix A, stored in the first kd+1 rows of the array.  On exit, the triangular factor U or L from the Cholesky factorization A = U^H * U or A = L * L^H of the band matrix A in the same storage format as A.
+   /// @param ldAB Column length of the array AB.  ldAB >= kd+1;
+   /// @ingroup TRF
+
+   template< typename real_t>
+   int_t pbtrf(const char uplo, const int_t n, const int_t kd, complex<real_t> * const AB, const int_t ldAB)
+   {
+      if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
+         return -1;
+      if ( n < 0)
+         return -2;
+      if (kd < 0 )
+         return -3;
+      if (ldAB < kd+1)
+         return -5;
+
+      if ( n == 0)
+         return 0;
+
+      const real_t one(1.0);
+      const real_t zero(0.0);
+      int_t kn, kld = ldAB-1;
+      real_t ajj;
+      complex<real_t> * ABj = AB;
+      complex<real_t> * ABjp1 = AB+ldAB;
+
+      if (uplo == 'U' || uplo == 'u')
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = real(ABj[kd]);
+            if (ajj <= zero || std::isnan(ajj))
+            {
+               ABj[kd] = ajj;
+               return j+1;
+            }
+            ABj[kd] = ajj = sqrt(ajj);
+
+            kn = std::min(kd, n-j-1);
+            if (kn > 0)
+            {
+               latl::scal(kn, one/ajj, ABjp1+kd-1, kld);
+               latl::lacgv(kn, ABjp1+kd-1, kld);
+               latl::her('U', kn, -one, ABjp1+kd-1, kld, ABjp1+kd, kld);
+               latl::lacgv(kn, ABjp1+kd, kld);
+            }
+            ABj += ldAB;
+            ABjp1 += ldAB;
+         }
+      }
+      else
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = real(ABj[0]);
+            if (ajj <= 0 || std::isnan(ajj))
+            {
+               ABj[0] = ajj;
+               return j+1;
+            }
+            ABj[0] = ajj = sqrt(ajj);
+            kn = std::min(kd, n-j-1);
+            if (kn > 0)
+            {
+               latl::scal(kn, one/ajj, ABj+1, 1);
+               latl::her('L', kn, -one, ABj+1, 1, ABjp1, kld);
+            }
+            ABj += ldAB;
+            ABjp1 += ldAB;
+         }
+      }
+      return 0;
+   }
+
+   /// @brief Computes the Cholesky factorization of a real symmetric positive definite band matrix A.
+   ///
+   /// The factorization has the form
+   ///
+   ///         A = U' * U   if uplo = 'U'
+   ///         A = L * L'   if uplo = 'L'
+   ///
+   /// where U is upper triangular, U' is the transpose of U, and L is lower triangular.
+   /// @return 0 if success
+   /// @return -i if the ith argument is invalid.
+   /// @return i+1 if the leading minor of order i is not positive definite.
+   /// @tparam real_t Floating point type.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param n Order of the matrix A.  n >= 0
+   /// @param kd The number of super-diagonals of the matrix A if uplo = 'U' or the number of sub-diagonals if uplo = 'L'.  kd >= 0
+   /// @param AB Real array size ldAB-by-n.  On entry, the upper or lower triangle of the symmetric band matrix A, stored in the first kd+1 rows of the array. On exit, the triangular factor U or L from the Cholesky factorization A = U' * U or A = L * L' of the band matrix A in the same storage format as A.
+   /// @param ldAB Column length of the array AB.  ldAB >= kd+1;
+   /// @param nb Block size.
    
    template< typename real_t>
-   int_t pbtrf(const char uplo, const int_t n, const int_t kd, real_t * const AB, const int_t ldAB, const int_t nb = 64)
+   int_t pbtrf(const char uplo, const int_t n, const int_t kd, real_t * const AB, const int_t ldAB, const int_t nb)
    {
       if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
          return -1;
@@ -57,7 +229,7 @@ namespace latl
          return 0;
       
       if (nb <= 1 || nb > kd)
-         return latl::pbtf2(uplo, n, kd, AB, ldAB);
+         return latl::pbtrf(uplo, n, kd, AB, ldAB);
       else
       {
          const real_t one(1.0);
@@ -81,7 +253,7 @@ namespace latl
             {
                ib = std::min(nb, n-i);
                
-               info = latl::potf2(uplo, ib, ABi+kd, kld);
+               info = latl::potrf(uplo, ib, ABi+kd, kld);
                
                if (info != 0)
                {
@@ -149,7 +321,7 @@ namespace latl
             {
                ib = std::min(nb, n-i);
                
-               info = latl::potf2(uplo, ib, ABi, kld);
+               info = latl::potrf(uplo, ib, ABi, kld);
                if (info != 0)
                {
                   return info+i;
@@ -222,11 +394,10 @@ namespace latl
    /// @param kd The number of super-diagonals of the matrix A if uplo = 'U' or the number of sub-diagonals if uplo = 'L'.  kd >= 0
    /// @param AB Complex array size ldAB-by-n.  On entry, the upper or lower triangle of the band matrix A, stored in the first kd+1 rows of the array. On exit, the triangular factor U or L from the Cholesky factorization A = U' * U or A = L * L' of the band matrix A in the same storage format as A.
    /// @param ldAB Column length of the array AB.  ldAB >= kd+1;
-   /// @param nb Block size, optional.  Default value is 64.
-   /// @ingroup TRF
+   /// @param nb Block size.
    
    template< typename real_t>
-   int_t pbtrf(const char uplo, const int_t n, const int_t kd, complex<real_t> * const AB, const int_t ldAB, const int_t nb = 64)
+   int_t pbtrf(const char uplo, const int_t n, const int_t kd, complex<real_t> * const AB, const int_t ldAB, const int_t nb)
    {
       if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
          return -1;
@@ -241,7 +412,7 @@ namespace latl
          return 0;
       
       if (nb <= 1 || nb > kd)
-         return latl::pbtf2(uplo, n, kd, AB, ldAB);
+         return latl::pbtrf(uplo, n, kd, AB, ldAB);
       else
       {
          const real_t one(1.0);
@@ -266,7 +437,7 @@ namespace latl
             {
                ib = std::min(nb, n-i);
                
-               info = latl::potf2(uplo, ib, ABi+kd, kld);
+               info = latl::potrf(uplo, ib, ABi+kd, kld);
                
                if (info != 0)
                {
@@ -333,7 +504,7 @@ namespace latl
             {
                ib = std::min(nb, n-i);
                
-               info = latl::potf2(uplo, ib, ABi, kld);
+               info = latl::potrf(uplo, ib, ABi, kld);
                if (info != 0)
                {
                   return info+i;
--- include/potrf.h
+++ include/potrf.h
@@ -11,7 +11,10 @@
 
 /// @file potrf.h Computes the Cholesky factorization of a symmetric positive definite matrix A.
 
-#include "potf2.h"
+#include "dot.h"
+#include "gemv.h"
+#include "scal.h"
+#include "lacgv.h"
 #include "syrk.h"
 #include "gemm.h"
 #include "trsm.h"
@@ -20,6 +23,168 @@
 
 namespace latl
 {
+   /// @brief Computes the Cholesky factorization of a real symmetric positive definite matrix A.
+   ///
+   /// The factorization has the form
+   ///
+   ///         A = U' * U  if uplo = 'U'
+   ///         A = L * L'  if uplo = 'L'
+   ///
+   /// where U is an upper triangular matrix and L is lower triangular.
+   /// @return 0 if success
+   /// @return -i if the ith argument is invalid.
+   /// @return i+1 if the leading minor of order i is not positive definite.
+   /// @tparam real_t Floating point type.
+   /// @param uplo Indicates whether the symmetric matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param n Order of the matrix A.  n >= 0
+   /// @param A Real symmetric matrix size ldA-by-n.  On entry, the matrix A.  On exit, the factor U or L from the Cholesky factorization A = U' * U or A = L * L'.
+   /// @param ldA Column length of matrix A. ldA >= n
+
+   template< typename real_t>
+   int_t potrf(const char uplo, const int_t n, real_t * A, const int_t ldA)
+   {
+      if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
+         return -1;
+      if (n < 0)
+         return -2;
+      if (ldA < n)
+         return -4;
+
+      if (n == 0)
+         return 0;
+
+      const real_t one(1.0);
+      const real_t zero(0.0);
+      real_t ajj;
+      real_t * Aj = A;
+      int_t info = 0;
+      if (uplo == 'U' || uplo == 'u')
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = Aj[j] - latl::dot(j, Aj, 1, Aj, 1);
+            if (ajj <= zero or std::isnan(ajj))
+            {
+               Aj[j] = ajj;
+               return j+1;
+            }
+            Aj[j] = ajj = std::sqrt(ajj);
+
+            if (j < n-1)
+            {
+               latl::gemv('T', j, n-j-1, -one, Aj+ldA, ldA, Aj, 1, one, Aj+ldA+j, ldA);
+               latl::scal(n-j-1, one/ajj, Aj+ldA+j, ldA);
+            }
+            Aj += ldA;
+         }
+      }
+      else
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = Aj[j] - latl::dot(j, A+j, ldA, A+j, ldA);
+            if (ajj <= zero || std::isnan(ajj))
+            {
+               Aj[j] = ajj;
+               return j+1;
+            }
+            Aj[j] = ajj = std::sqrt(ajj);
+
+            if (j < n-1)
+            {
+               latl::gemv('N', n-j-1, j, -one, A+j+1, ldA, A+j, ldA, one, Aj+j+1, 1);
+               latl::scal(n-j-1, one/ajj, Aj+j+1, 1);
+            }
+            Aj += ldA;
+         }
+      }
+      return info;
+   }
+
+   /// @brief Computes the Cholesky factorization of a complex Hermitian positive definite matrix A.
+   ///
+   /// The factorization has the form
+   ///
+   ///         A = U^H * U if uplo = 'U'
+   ///         A = L * L^H if uplo = 'L'
+   ///
+   /// where U is an upper triangular matrix and L is lower triangular.
+   /// @return 0 if success
+   /// @return -i if the ith argument is invalid.
+   /// @return i+1 if the leading minor of order i is not positive definite.
+   /// @tparam real_t Floating point type.
+   /// @param uplo Indicates whether the Hermitian matrix A is stored as upper triangular or lower triangular.  The other triangular part of A is not referenced.
+   /// @param n Order of the matrix A.  n >= 0
+   /// @param A Hermitian matrix size ldA-by-n.  On entry, the matrix A.  On exit, the factor U or L from the Cholesky factorization A = U^H * U or A = L * L^H.
+   /// @param ldA Column length of matrix A. ldA >= n
+
+   template< typename real_t>
+   int_t potrf(const char uplo, const int_t n, complex<real_t> * A, const int_t ldA)
+   {
+      if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
+         return -1;
+      if (n < 0)
+         return -2;
+      if (ldA < n)
+         return -4;
+
+      if (n == 0)
+         return 0;
+
+      const real_t one(1.0);
+      const complex<real_t> onec(1.0);
+      const real_t zero(0.0);
+      real_t ajj;
+      complex<real_t> * Aj = A;
+      int_t info = 0;
+      if (uplo == 'U' || uplo == 'u')
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = real(Aj[j] - latl::dotc(j, Aj, 1, Aj, 1));
+            if (ajj <= zero or std::isnan(ajj))
+            {
+               Aj[j] = ajj;
+               return j+1;
+            }
+            Aj[j] = ajj = std::sqrt(ajj);
+
+            if (j < n-1)
+            {
+               latl::lacgv(j, Aj, 1);
+               latl::gemv('T', j, n-j-1, -onec, Aj+ldA, ldA, Aj, 1, onec, Aj+ldA+j, ldA);
+               latl::lacgv(j, Aj, 1);
+               latl::scal(n-j-1, one/ajj, Aj+ldA+j, ldA);
+            }
+            Aj += ldA;
+         }
+      }
+      else
+      {
+         for (int_t j = 0; j < n; ++j)
+         {
+            ajj = real(Aj[j] - latl::dotc(j, A+j, ldA, A+j, ldA));
+            if (ajj <= zero || std::isnan(ajj))
+            {
+               Aj[j] = ajj;
+               return j+1;
+            }
+            Aj[j] = ajj = std::sqrt(ajj);
+
+            if (j < n-1)
+            {
+               latl::lacgv(j, A+j, ldA);
+               latl::gemv('N', n-j-1, j, -onec, A+j+1, ldA, A+j, ldA, onec, Aj+j+1, 1);
+               latl::lacgv(j, A+j, ldA);
+               latl::scal(n-j-1, one/ajj, Aj+j+1, 1);
+            }
+            Aj += ldA;
+         }
+      }
+      return info;
+   }
+
+
    /// @brief Computes the Cholesky factorization of a real symmetric positive definite matrix A.
    ///
    /// The factorization has the form
@@ -36,11 +201,10 @@ namespace latl
    /// @param n Order of the matrix A.  n >= 0
    /// @param A Real symmetric matrix size ldA-by-n.  On entry, the matrix A.  On exit, the factor U or L from the Cholesky factorization A = U' * U or A = L * L'.
    /// @param ldA Column length of matrix A. ldA >= n
-   /// @param nb Block size, optional.  Default value is 64.
-   /// @ingroup TRF
+   /// @param nb Block size.
    
    template< typename real_t>
-   int_t potrf(const char uplo, const int_t n, real_t * const A, const int_t ldA, const int_t nb = 64)
+   int_t potrf(const char uplo, const int_t n, real_t * const A, const int_t ldA, const int_t nb)
    {
       if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
          return -1;
@@ -53,7 +217,7 @@ namespace latl
          return 0;
       
       if (nb <= 1 || nb >= n)
-         return latl::potf2(uplo, n, A, ldA);
+         return latl::potrf(uplo, n, A, ldA);
       else
       {
          int_t info = 0, jb;
@@ -65,7 +229,7 @@ namespace latl
             {
                jb = std::min( nb, n-j);
                latl::syrk('U', 'T', jb, j, -one, Aj, ldA, one, Aj+j, ldA);
-               info = latl::potf2('U', jb, Aj+j, ldA);
+               info = latl::potrf('U', jb, Aj+j, ldA);
                if (info != 0)
                   return info+j;
                if (j+jb < n)
@@ -82,7 +246,7 @@ namespace latl
             {
                jb = std::min(nb, n-j);
                latl::syrk('L', 'N', jb, j, -one, A+j, ldA, one, Aj+j, ldA);
-               info = latl::potf2('L', jb, Aj+j, ldA);
+               info = latl::potrf('L', jb, Aj+j, ldA);
                if (info != 0)
                   return info+j;
                if (j+jb < n)
@@ -113,11 +277,10 @@ namespace latl
    /// @param n Order of the matrix A.  n >= 0
    /// @param A Hermitian matrix size ldA-by-n.  On entry, the matrix A.  On exit, the factor U or L from the Cholesky factorization A = U^H * U or A = L * L^H.
    /// @param ldA Column length of matrix A. ldA >= n
-   /// @param nb Block size, optional.  Default value is 64.
-   /// @ingroup TRF
-   
+   /// @param nb Block size.
+
    template< typename real_t>
-   int_t potrf(const char uplo, const int_t n, complex<real_t> * const A, const int_t ldA, const int_t nb = 64)
+   int_t potrf(const char uplo, const int_t n, complex<real_t> * const A, const int_t ldA, const int_t nb)
    {
       if (uplo != 'U' && uplo != 'L' && uplo != 'u' && uplo != 'l')
          return -1;
@@ -130,7 +293,7 @@ namespace latl
          return 0;
       
       if (nb <= 1 || nb >= n)
-         return latl::potf2(uplo, n, A, ldA);
+         return latl::potrf(uplo, n, A, ldA);
       else
       {
          int_t info = 0, jb;
@@ -143,7 +306,7 @@ namespace latl
             {
                jb = std::min( nb, n-j);
                latl::herk('U', 'C', jb, j, -one, Aj, ldA, one, Aj+j, ldA);
-               info = latl::potf2('U', jb, Aj+j, ldA);
+               info = latl::potrf('U', jb, Aj+j, ldA);
                if (info != 0)
                   return info+j;
                if (j+jb < n)
@@ -160,7 +323,7 @@ namespace latl
             {
                jb = std::min(nb, n-j);
                latl::herk('L', 'N', jb, j, -one, A+j, ldA, one, Aj+j, ldA);
-               info = latl::potf2('L', jb, Aj+j, ldA);
+               info = latl::potrf('L', jb, Aj+j, ldA);
                if (info != 0)
                   return info+j;
                if (j+jb < n)
