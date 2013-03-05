594c69f19c71b7358b412720a27952e9601f8eac
Rodney James <rodney.james@ucdenver.edu>
2013-03-05 07:07:51 -0700

    updated getc2

--- include/getc2.h
+++ include/getc2.h
@@ -12,6 +12,8 @@
 /// @file getc2.h Computes an LU factorization with complete pivoting of an n-by-n matrix.
 
 #include <limits>
+#include <cmath>
+#include <algorithm>
 #include "labad.h"
 #include "swap.h"
 #include "ger.h"
@@ -19,197 +21,166 @@
 
 namespace latl
 {
-   /// @brief Computes an LU factorization with complete pivoting of an n-by-n matrix.
+   /// @brief Computes an LU factorization with complete pivoting of an n-by-n real matrix.
    ///
    /// The factorization has the form
    /// A = P * L * U * Q
-   /// where P and Q are permutation matrices.  L is lower triangular with unit diagonal elements and U is upper triangular.
-   ///
+   /// where P and Q are permutation matrices.  L is lower triangular with unit diagonal
+   /// elements and U is upper triangular.
+   /// @return 0 if success.
+   /// @return 1 if diagonal element was reset to minimum value to avoid singularity.
    /// @tparam real_t Floating point type.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Real array size ldA-by-n.  On entry, the n-by-n matrix to be factored.  On exit, contains the factors L and U from the factorization.  The unit diagonal elements are not stored.  If U(k,k) appears to be less than smin, U(k,k) is given the value of smin, giving a nonsingular perturbed system.
+   /// @param A Real n-by-n matrix.  On entry, the n-by-n matrix to be factored.
+   /// On exit, contains the factors L and U from the factorization.  The unit diagonal elements
+   /// are not stored.
    /// @param ldA The column length of the array A.  ldA >= n
-   /// @param IPIV Integer array size n.  Contains pivot indices; for 0 <= i <= n-1, row i of the matrix has been exchanged with row IPIV[i].
-   /// @param JPIV Integer array size n.  Contains pivot indices; for 0 <= j <= n-1, column j of the matrix has been exchanged with column JPIV[j].
-   /// @ingroup TRF
+   /// @param ipiv Integer array size n.  Contains pivot indices; for 0 <= i <= n-1,
+   /// row i of the matrix has been exchanged with row ipiv[i].
+   /// @param jpiv Integer array size n.  Contains pivot indices; for 0 <= j <= n-1,
+   /// column j of the matrix has been exchanged with column jpiv[j].
    
    template<typename real_t>
-   int getc2(const int_t n, real_t * const A, const int_t ldA, int_t * const IPIV, int_t * const JPIV)
+   int getc2(int_t n, real_t *A, int_t ldA, int_t *ipiv, int_t *jpiv)
    {
       using std::numeric_limits;
-
-      if (n < 0)
-         return -1;
-      if (ldA < n)
-         return -3;
-      
+      using std::abs;
+      using std::max;
       const real_t one(1.0);
       const real_t zero(0.0);
       const real_t eps = numeric_limits<real_t>::epsilon();
+
       real_t smlnum = numeric_limits<real_t>::min()/eps;
       real_t bignum = one/smlnum;
-      latl::labad(smlnum, bignum);
-      
-      int_t ipv, jpv, info = 0;
-      real_t xmax, smin;
-      real_t * Ai = A;
-      real_t * Aij = A;
-      for (int_t i = 0; i < n-1; ++i)
+      labad(smlnum, bignum);
+
+      int info=0;
+      int_t ipv=0;
+      int_t jpv=0;
+      real_t smin;
+
+
+      for(int_t i=0; i<n-1;i++)
       {
-         xmax = zero;
-         for (int_t ip = i; ip < n; ++ip)
+         real_t xmax=zero;
+         for(int_t jp=i;jp<n;jp++)
          {
-            Ai = A + ip;
-            for (int_t jp = i; jp < n; ++jp)
+            for(int_t ip=i;ip<n;ip++)
             {
-               Aij = Ai + ldA*jp;
-               if (abs(Aij[0]) >= xmax)
+               real_t a=A[ip+jp*ldA];
+               if(abs(a)>=xmax)
                {
-                  xmax = abs(Aij[0]);
-                  ipv = ip;
-                  jpv = jp;
+                  xmax=abs(a);
+                  ipv=ip;
+                  jpv=jp;
                }
             }
          }
-         if ( i == 0)
+         if(i==0)
+            smin=max(eps*xmax,smlnum);
+         if(ipv!=i)
+            swap(n,A+ipv,ldA,A+i,ldA);
+         ipiv[i]=ipv;
+         if(jpv!=i)
+            swap(n,A+ldA*jpv,1,A+ldA*i,1);
+         jpiv[i]=jpv;
+         if(abs(A[i+i*ldA])<smin)
          {
-            smin = std::max(eps*xmax, smlnum);
+            info=1;
+            A[i+i*ldA]=smin;
          }
-         
-         if (ipv != i)
-         {
-            latl::swap(n, A+ipv, ldA, A+i, ldA);
-         }
-         IPIV[i] = ipv;
-         
-         if (jpv != i)
-         {
-            latl::swap(n, A+ldA*jpv, 1, A+ldA*i, 1);
-         }
-         JPIV[i] = jpv;
-         
-         Aij = A+i+ldA*i;
-         if (abs(Aij[0]) < smin)
-         {
-            if (info == 0)
-               info = i+1;
-            Aij[0] = smin;
-         }
-         
-         for (int_t k = 1; k < (n-i); ++k)
-         {
-            Aij[k] = Aij[k] / Aij[0];
-         }
-         latl::ger(n-i-1, n-i-1, -one, Aij+1, 1, Aij+ldA, ldA, Aij+1+ldA, ldA);
+         for(int_t j=i+1;j<n;j++)
+            A[j+i*ldA]=A[j+i*ldA]/A[i+i*ldA];
+         ger(n-i-1,n-i-1,-one,A+i+1+i*ldA,1,A+i+(i+1)*ldA,ldA,A+i+1+(i+1)*ldA,ldA);
       }
-      
-      Aij = A+ldA*(n-1)+(n-1);
-      if (abs(Aij[0]) < smin)
+      if(abs(A[n-1+(n-1)*ldA])<smin)
       {
-         if (info == 0)
-            info = n;
-         Aij[0] = smin;
+         info=1;
+         A[n-1+(n-1)*ldA]=smin;
       }
-      JPIV[n-1] = IPIV[n-1] = n-1;
-      
+      jpiv[n-1]=ipiv[n-1]=n-1;
       return info;
    }
    
-   /// @brief Computes an LU factorization with complete pivoting of an n-by-n matrix.
+   /// @brief Computes an LU factorization with complete pivoting of an n-by-n complex matrix.
    ///
    /// The factorization has the form
    /// A = P * L * U * Q
-   /// where P and Q are permutation matrices.  L is lower triangular with unit diagonal elements and U is upper triangular.
-   ///
+   /// where P and Q are permutation matrices.  L is lower triangular with unit diagonal
+   /// elements and U is upper triangular.
+   /// @return 0 if success.
+   /// @return 1 if diagonal element was reset to minimum value to avoid singularity.
    /// @tparam real_t Floating point type.
    /// @param n Order of the matrix A.  n >= 0
-   /// @param A Complex array size ldA-by-n.  On entry, the n-by-n matrix to be factored.  On exit, contains the factors L and U from the factorization.  The unit diagonal elements are not stored.  If U(k,k) appears to be less than smin, U(k,k) is given the value of smin, giving a nonsingular perturbed system.
+   /// @param A Complex n-by-n matrix.  On entry, the n-by-n matrix to be factored.
+   /// On exit, contains the factors L and U from the factorization.  The unit diagonal elements
+   /// are not stored.
    /// @param ldA The column length of the array A.  ldA >= n
-   /// @param IPIV Integer array size n.  Contains pivot indices; for 0 <= i <= n-1, row i of the matrix has been exchanged with row IPIV[i].
-   /// @param JPIV Integer array size n.  Contains pivot indices; for 0 <= j <= n-1, column j of the matrix has been exchanged with column JPIV[j].
-   /// @ingroup TRF
-   
+   /// @param ipiv Integer array size n.  Contains pivot indices; for 0 <= i <= n-1,
+   /// row i of the matrix has been exchanged with row ipiv[i].
+   /// @param jpiv Integer array size n.  Contains pivot indices; for 0 <= j <= n-1,
+   /// column j of the matrix has been exchanged with column jpiv[j].
+
    template<typename real_t>
-   int getc2(const int_t n, complex<real_t> * const A, const int_t ldA, int_t * const IPIV, int_t * const JPIV)
+   int getc2(int_t n, complex<real_t> *A, int_t ldA, int_t *ipiv, int_t *jpiv)
    {
       using std::numeric_limits;
-
-      if (n < 0)
-         return -1;
-      if (ldA < n)
-         return -3;
-      
+      using std::abs;
+      using std::max;
       const real_t one(1.0);
-      const complex<real_t> cxone(1.0);
+      const complex<real_t> minus_one(-1.0);
       const real_t zero(0.0);
       const real_t eps = numeric_limits<real_t>::epsilon();
+
       real_t smlnum = numeric_limits<real_t>::min()/eps;
       real_t bignum = one/smlnum;
-      latl::labad(smlnum, bignum);
-      
-      int_t ipv, jpv, info = 0;
-      real_t xmax;
-      complex<real_t> cmin;
-      complex<real_t> * Ai = A;
-      complex<real_t> * Aij = A;
-      for (int_t i = 0; i < n-1; ++i)
+      labad(smlnum, bignum);
+
+      int info=0;
+      int_t ipv=0;
+      int_t jpv=0;
+      real_t smin;
+
+
+      for(int_t i=0; i<n-1;i++)
       {
-         xmax = zero;
-         for (int_t ip = i; ip < n; ++ip)
+         real_t xmax=zero;
+         for(int_t jp=i;jp<n;jp++)
          {
-            Ai = A + ip;
-            for (int_t jp = i; jp < n; ++jp)
+            for(int_t ip=i;ip<n;ip++)
             {
-               Aij = Ai + ldA*jp;
-               if (abs(Aij[0]) >= xmax)
+               complex<real_t> a=A[ip+jp*ldA];
+               if(abs(a)>=xmax)
                {
-                  xmax = abs(Aij[0]);
-                  ipv = ip;
-                  jpv = jp;
+                  xmax=abs(a);
+                  ipv=ip;
+                  jpv=jp;
                }
             }
          }
-         if ( i == 0)
+         if(i==0)
+            smin=max(eps*xmax,smlnum);
+         if(ipv!=i)
+            swap(n,A+ipv,ldA,A+i,ldA);
+         ipiv[i]=ipv;
+         if(jpv!=i)
+            swap(n,A+ldA*jpv,1,A+ldA*i,1);
+         jpiv[i]=jpv;
+         if(abs(A[i+i*ldA])<smin)
          {
-            cmin = std::max(eps*xmax, smlnum);
+            info=1;
+            A[i+i*ldA]=smin;
          }
-         
-         if (ipv != i)
-         {
-            latl::swap(n, A+ipv, ldA, A+i, ldA);
-         }
-         IPIV[i] = ipv;
-         
-         if (jpv != i)
-         {
-            latl::swap(n, A+ldA*jpv, 1, A+ldA*i, 1);
-         }
-         JPIV[i] = jpv;
-         
-         Aij = A+i+ldA*i;
-         if (abs(Aij[0]) < real(cmin))
-         {
-            if (info == 0)
-               info = i+1;
-            Aij[0] = cmin;
-         }
-         
-         for (int_t k = 1; k < (n-i); ++k)
-         {
-            Aij[k] = Aij[k] / Aij[0];
-         }
-         latl::ger(n-i-1, n-i-1, -cxone, Aij+1, 1, Aij+ldA, ldA, Aij+1+ldA, ldA);
+         for(int_t j=i+1;j<n;j++)
+            A[j+i*ldA]=A[j+i*ldA]/A[i+i*ldA];
+         ger(n-i-1,n-i-1,minus_one,A+i+1+i*ldA,1,A+i+(i+1)*ldA,ldA,A+i+1+(i+1)*ldA,ldA);
       }
-      
-      ipv = (n-1)*(n-1);
-      if (abs(A[ipv]) < real(cmin))
+      if(abs(A[n-1+(n-1)*ldA])<smin)
       {
-         if (info == 0)
-            info = n;
-         A[ipv] = cmin;
+         info=1;
+         A[n-1+(n-1)*ldA]=smin;
       }
-      JPIV[n-1] = IPIV[n-1] = n-1;
-      
+      jpiv[n-1]=ipiv[n-1]=n-1;
       return info;
    }
 }
