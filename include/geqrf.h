63bafafb50dd4af2d63f49e0223b07e930c9e118
Rodney James <rodney.james@ucdenver.edu>
2013-03-26 17:49:19 -0600

    added GEQRF, fixed GEQR2

--- include/geqrf.h
+++ include/geqrf.h
@@ -1,24 +1,25 @@
 //
-//  geqr2.h
+//  geqrf.h
 //  Linear Algebra Template Library
 //
 //  Created by Rodney James on 3/26/13.
 //  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
 //
 
-#ifndef _geqr2_h
-#define _geqr2_h
+#ifndef _geqrf_h
+#define _geqrf_h
 
-/// @file geqr2.h Computes a QR factorization of a matrix.
+/// @file geqrf.h Computes a blocked QR factorization of a matrix.
 
 #include <algorithm>
-#include "larf.h"
-#include "larfg.h"
+#include "geqr2.h"
+#include "larfb.h"
+#include "larft.h"
 #include "latl.h"
 
 namespace LATL
 {
-   /// @brief Computes a QR factorization of a real matrix A.
+   /// @brief Computes a blocked QR factorization of a real matrix A.
    ///
    /// The matrix Q is represented as a product of elementary reflectors
    ///
@@ -45,46 +46,52 @@ namespace LATL
    /// @param ldA Column length of the matrix A.  ldA>=m.
    /// @param[out] tau Real vector of length min(m,n).
    /// The scalar factors of the elementary reflectors.
-   /// @param w Workspace vector of length n (optional).
+   /// @param nb Blocking factor (optional).
+   /// @param w Workspace vector of length nb*(n+1) (optional).
    /// If not used, workspace is managed internally.
 
    template<typename real_t>
-   int GRQR2(int_t m, int_t n, real_t *A, int_t ldA, real_t *tau, real_t *w=NULL)
+   int GRQRF(int_t m, int_t n, complex<real_t> *A, int_t ldA, complex<real_t> *tau, int_t nb=80, complex<real_t> *w=NULL)
    {
       using std::min;
-      const real_t one(1.0);
+      const complex<real_t> one(1.0);
       if(m<0)
          return -1;
       else if(n<0)
          return -2;
       else if(ldA<m)
          return -4;
+      else if(nb<1)
+         return -6;
 
       int_t k=min(m,n);
 
+      if(k==0)
+         return 0;
+      
       bool allocate=(w==NULL)?1:0;
       if(allocate)
-         w=new real_t[n];
-      real_t *B=A;
-      for(int_t i=0;i<k;i++)
+         w=new complex<real_t>[n*nb+nb];
+      complex<real_t> *B=A;
+      for(int_t i=0;i<k;i+=nb)
       {
-         B+=ldA;
-         LARFG(m-i,A[i],&(A[i+1]),1,tau[i]);
-         if(i<n-1)
+         int_t ib=min(k-i,nb);
+         B+=ib*ldA;
+         GEQR2(m-i,ib,A+i,ldA,tau,w);
+         if(i+ib<n-1)
          {
-            real_t alpha=A[i];
-            A[i]=one;
-            LARF('L',m-i,n-i-1,A[i],1,tau[i],B+i,ldA,w);
-            A[i]=alpha;
+            LARFT('F','C',m-i,ib,A+i,ldA,tau,w,ib);
+            LARFB('L','T','F','C',m-i,n-i-ib,ib,A+i,ldA,w,ib,B+i,ldA,w+ib*ib);
          }
          A=B;
+         tau+=ib;
       }
       if(allocate)
          delete [] w;
       return 0;
    }
 
-   /// @brief Computes a QR factorization of a complex matrix A.
+   /// @brief Computes a blocked QR factorization of a complex matrix A.
    ///
    /// The matrix Q is represented as a product of elementary reflectors
    ///
@@ -111,13 +118,50 @@ namespace LATL
    /// @param ldA Column length of the matrix A.  ldA>=m.
    /// @param[out] tau Complex vector of length min(m,n).
    /// The scalar factors of the elementary reflectors.
-   /// @param w Workspace vector of length n (optional).
+   /// @param nb Blocking factor (optional).
+   /// @param w Workspace vector of length nb*(n+1) (optional).
    /// If not used, workspace is managed internally.
 
    template<typename real_t>
-   int GEQR2(int_t m, int_t n, complex<real_t> *A, int_t ldA, complex<real_t> *tau, complex<real_t> *w=NULL)
+   int GRQRF(int_t m, int_t n, real_t *A, int_t ldA, real_t *tau, int_t nb=80, real_t *w=NULL)
    {
-      return GEQR2< complex<real_t> >(m,n,A,ldA,tau,w);
+      using std::min;
+      const real_t one(1.0);
+      if(m<0)
+         return -1;
+      else if(n<0)
+         return -2;
+      else if(ldA<m)
+         return -4;
+      else if(nb<1)
+         return -6;
+
+      int_t k=min(m,n);
+
+      if(k==0)
+         return 0;
+
+      bool allocate=(w==NULL)?1:0;
+      if(allocate)
+         w=new real_t[n*nb+nb];
+      real_t *B=A;
+      for(int_t i=0;i<k;i+=nb)
+      {
+         int_t ib=min(k-i,nb);
+         B+=ib*ldA;
+         GEQR2(m-i,ib,A+i,ldA,tau,w);
+         if(i+ib<n-1)
+         {
+            LARFT('F','C',m-i,ib,A+i,ldA,tau,w,ib);
+            LARFB('L','C','F','C',m-i,n-i-ib,ib,A+i,ldA,w,ib,B+i,ldA,w+ib*ib);
+         }
+         A=B;
+         tau+=ib;
+      }
+      if(allocate)
+         delete [] w;
+      return 0;
    }
 }
 #endif
+
