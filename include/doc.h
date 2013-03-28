cdd3345486ff9e0d73aeed8da15619fc11a296fc
Rodney James <rodney.james@ucdenver.edu>
2013-03-28 15:58:52 -0600

    added doc.h (doxygen info)

---
+++ include/doc.h
@@ -0,0 +1,29 @@
+//
+//  doc.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 3/28/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _doc_h
+#define _doc_h
+
+/// @file doc.h Contains general documentation for doxygen.
+
+/// @mainpage
+/// @section intro Introduction
+/// The LATL is a precision neutral generic dense linear algebra
+/// library based on <A HREF="http://netlib.org/lapack/">LAPACK</A>
+/// written in C++ using function templates.
+///
+/// @section code Download
+/// Source code and git access is available on GitHub
+/// <A HREF="http://github.com/projects/latl/latl">here</A>.
+///
+/// @defgroup BLAS BLAS routine
+/// @defgroup AUX Auxiliary routine
+/// @defgroup COMP Computational routine
+/// @defgroup DRIV Driver routine
+
+#endif
