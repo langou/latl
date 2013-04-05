97a07b06aef145d8e3a49aa5a4a43f8b3fcace34
Rodney James <rodney.james@ucdenver.edu>
2013-04-05 14:06:05 -0600

    fixed GBTRF

--- include/gbtrf.h
+++ include/gbtrf.h
@@ -11,6 +11,7 @@
 
 /// @file gbtrf.h Computes an LU factorization of an m-by-n band matrix A using partial pivoting with row interchanges using blocked algorithm.
 
+#include <algorithm>
 #include "copy.h"
 #include "gemm.h"
 #include "laswp.h"
@@ -40,6 +41,8 @@ namespace LATL
    template< typename real_t>
    int_t GBTRF(const int_t m, const int_t n, const int_t kL, const int_t kU, real_t * const AB, const int_t ldAB, int_t * const ipiv, const int_t nb)
    {
+      using std::min;
+      using std::max;
       if (m < 0)
          return -1;
       if (n < 0)
@@ -56,7 +59,7 @@ namespace LATL
       
       int_t info = 0;
       if (nb > kL || nb <= 1)
-         info = LATL::GBTF2(m, n, kL, kU, AB, ldAB, ipiv);
+         info = GBTF2(m, n, kL, kU, AB, ldAB, ipiv);
       else
       {
          const real_t zero(0.0);
@@ -84,7 +87,7 @@ namespace LATL
             temp31 += ldWork;
          }
          real_t * ABj = AB;
-         for (int_t j = kU+1; j < std::min(kV, n); ++j)
+         for (int_t j = kU+1; j < min(kV, n); ++j)
          {
             ABj = AB+ldAB*j;
             for (int_t i = kV-j; i < kL; ++i)
@@ -95,12 +98,12 @@ namespace LATL
          
          int_t jU = 0, jb, i2, i3, km, nw, jp, jm;
          real_t * ABjj = AB;
-         for (int_t j = 0; j < std::min(m, n); j+=nb)
+         for (int_t j = 0; j < min(m, n); j+=nb)
          {
             ABj = AB+ldAB*j;
-            jb = std::min(nb, std::min(m, n)-j);
-            i2 = std::min(kL-jb, m-j-jb);
-            i3 = std::min(jb, m-j-kL);
+            jb = min(nb, min(m, n)-j);
+            i2 = min(kL-jb, m-j-jb);
+            i3 = min(jb, m-j-kL);
             for (int_t jj = j; jj < j+jb; ++jj)
             {
                if (jj+kV < n)
@@ -113,30 +116,30 @@ namespace LATL
                }
                
                ABjj = AB+ldAB*jj;
-               km = std::min(kL, m-jj-1);
-               jp = LATL::IMAX(km+1, ABjj+kV, 1);
+               km = min(kL, m-jj-1);
+               jp = IMAX(km+1, ABjj+kV, 1);
                ipiv[jj] = jp+jj-j;
                if (ABjj[kV+jp] != zero)
                {
-                  jU = std::max(jU, std::min(jj+kU+jp, n-1));
+                  jU = max(jU, min(jj+kU+jp, n-1));
                   if (jp != 0)
                   {
                      if ((jp+jj) < (j+kL))
                      {
-                        LATL::SWAP(jb, ABj+kV+jj-j, ldAB-1, ABj+kV+jp+jj-j, ldAB-1);
+                        SWAP(jb, ABj+kV+jj-j, ldAB-1, ABj+kV+jp+jj-j, ldAB-1);
                      }
                      else
                      {
-                        LATL::SWAP(jj-j, ABj+kV+jj-j, ldAB-1, Work31+jp+jj-j-kL, ldWork);
-                        LATL::SWAP(j+jb-jj, ABjj+kV, ldAB-1, ABjj+kV+jp, ldAB-1);
+                        SWAP(jj-j, ABj+kV+jj-j, ldAB-1, Work31+jp+jj-j-kL, ldWork);
+                        SWAP(j+jb-jj, ABjj+kV, ldAB-1, ABjj+kV+jp, ldAB-1);
                      }
                   }
-                  LATL::SCAL(km, one/ABjj[kV], ABjj+kV+1, 1);
+                  SCAL(km, one/ABjj[kV], ABjj+kV+1, 1);
                   
-                  jm = std::min(jU, j+jb-1);
+                  jm = min(jU, j+jb-1);
                   if (jm > jj)
                   {
-                     LATL::GER(km, jm-jj, -one, ABjj+kV+1, 1, ABjj+ldAB+kV-1, ldAB-1, ABjj+ldAB+kV, ldAB-1);
+                     GER(km, jm-jj, -one, ABjj+kV+1, 1, ABjj+ldAB+kV-1, ldAB-1, ABjj+ldAB+kV, ldAB-1);
                   }
                }
                else
@@ -144,10 +147,10 @@ namespace LATL
                   if (info == 0)
                      info = jj+1;
                }
-               nw = std::min(jj-j+1, i3);
+               nw = min(jj-j+1, i3);
                if (nw > 0)
                {
-                  LATL::COPY(nw, ABjj+kV+kL-jj+j, 1, Work31+ldWork*(jj-j), 1);
+                  COPY(nw, ABjj+kV+kL-jj+j, 1, Work31+ldWork*(jj-j), 1);
                }
             }
             
@@ -155,9 +158,9 @@ namespace LATL
             {
                int_t j2, j3, k2;
                
-               j2 = std::min(jU-j+1, kV)-jb;
-               j3 = std::max(0, jU-j-kV+1);
-               LATL::LASWP(j2, ABj+ldAB*jb+kV-jb, ldAB-1, 0, jb-1, ipiv+j);
+               j2 = min(jU-j+1, kV)-jb;
+               j3 = max(0, jU-j-kV+1);
+               LASWP(j2, ABj+ldAB*jb+kV-jb, ldAB-1, 0, jb-1, ipiv+j);
                
                for (int_t i = j; i < j+jb; ++i)
                {
@@ -188,14 +191,13 @@ namespace LATL
                if (j2 > 0)
                {
                   real_t * ABjjb = ABj + ldAB*jb;
-                  LATL::TRSM('L', 'L', 'N', 'U', jb, j2, one, ABj+kV, ldAB-1, ABjjb+kV-jb, ldAB-1);
+                  TRSM('L', 'L', 'N', 'U', jb, j2, one, ABj+kV, ldAB-1, ABjjb+kV-jb, ldAB-1);
                   if (i2 > 0)
-                     LATL::GEMM('N', 'N', i2, j2, jb, -one, ABj+kV+jb, ldAB-1, ABjjb+kV-jb, ldAB-1, one, ABjjb+kV, ldAB-1);
+                     GEMM('N', 'N', i2, j2, jb, -one, ABj+kV+jb, ldAB-1, ABjjb+kV-jb, ldAB-1, one, ABjjb+kV, ldAB-1);
                   if (i3 > 0)
-                     LATL::GEMM('N', 'N', i3, j2, jb, -one, Work31, ldWork, ABjjb+kV-jb, ldAB-1, one, ABjjb+kV+kL-jb, ldAB-1);
+                     GEMM('N', 'N', i3, j2, jb, -one, Work31, ldWork, ABjjb+kV-jb, ldAB-1, one, ABjjb+kV+kL-jb, ldAB-1);
                }
                
-               std::cout << "j3: " << j3 << std::endl;
                if (j3 > 0)
                {
                   temp13 = Work13;
@@ -211,15 +213,15 @@ namespace LATL
                   }
                   
                   ABjkv = ABj+ldAB*kV;
-                  LATL::TRSM('L', 'L', 'N', 'U', jb, j3, one, ABj+kV, ldAB-1, Work13, ldWork);
+                  TRSM('L', 'L', 'N', 'U', jb, j3, one, ABj+kV, ldAB-1, Work13, ldWork);
                   
                   if (i2 > 0)
                   {
-                     LATL::GEMM('N', 'N', i2, j3, jb, -one, ABj+kV+jb, ldAB-1, Work13, ldWork, one, ABjkv+jb+1, ldAB-1);
+                     GEMM('N', 'N', i2, j3, jb, -one, ABj+kV+jb, ldAB-1, Work13, ldWork, one, ABjkv+jb+1, ldAB-1);
                   }
                   if (i3 > 0)
                   {
-                  latl:GEMM('N', 'N', i3, j3, jb, -one, Work31, ldWork, Work13, ldWork, one, ABjkv+kL, ldAB-1);
+                  GEMM('N', 'N', i3, j3, jb, -one, Work31, ldWork, Work13, ldWork, one, ABjkv+kL, ldAB-1);
                   }
                   
                   temp13 = Work13;
@@ -249,17 +251,17 @@ namespace LATL
                {
                   if (jp+jj < j+kL)
                   {
-                     LATL::SWAP(jj-j, ABj+kV+jj-j, ldAB-1, ABj+kV+jp+jj-j, ldAB-1);
+                     SWAP(jj-j, ABj+kV+jj-j, ldAB-1, ABj+kV+jp+jj-j, ldAB-1);
                   }
                   else
                   {
-                     LATL::SWAP(jj-j, ABj+kV+jj-j, ldAB-1, Work31+jp+jj-j-kL, ldWork);
+                     SWAP(jj-j, ABj+kV+jj-j, ldAB-1, Work31+jp+jj-j-kL, ldWork);
                   }
                }
-               nw = std::min(i3, jj-j+1);
+               nw = min(i3, jj-j+1);
                if (nw > 0)
                {
-                  LATL::COPY(nw, Work31+(jj-j)*ldWork, 1, AB+ldAB*jj+kV+kL-jj+j, 1);
+                  COPY(nw, Work31+(jj-j)*ldWork, 1, AB+ldAB*jj+kV+kL-jj+j, 1);
                }
             }
          }
@@ -288,9 +290,9 @@ namespace LATL
    /// @ingroup COMP
 
    template< typename real_t>
-   int_t GBTRF(const int_t m, const int_t n, const int_t kL, const int_t kU, complex<real_t> * const AB, const int_t ldAB, int_t * const ipiv, const int_t nb, complex<real_t> *W=NULL)
+   int_t GBTRF(const int_t m, const int_t n, const int_t kL, const int_t kU, complex<real_t> * const AB, const int_t ldAB, int_t * const ipiv, const int_t nb)
    {
-      return LATL::GBTRF< complex<real_t> > (m, n, kL, kU, AB, ldAB, ipiv, nb, W);
+      return GBTRF< complex<real_t> > (m, n, kL, kU, AB, ldAB, ipiv, nb);
    }
 }
 
