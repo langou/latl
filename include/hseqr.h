6c803f898839228ce5a55eb096a547d1b3f27208
Rodney James <rodney.james@ucdenver.edu>
2013-05-01 10:45:07 -0600

    added HSEQR

---
+++ include/hseqr.h
@@ -0,0 +1,25 @@
+//
+//  hseqr.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 5/1/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _hseqr_h
+#define _hseqr_h
+
+/// @file hseqr.h Computes eigenvalues of a Hessenberg matrix.
+
+#include "latl.h"
+
+namespace LATL
+{
+   template<typename real_t>
+   int HSEQR(char job,char compZ,int_t n,real_t *H,int_t ldH,real_t *wr,real_t *wi,real_t *Z,int_t ldZ)
+   {
+
+   }
+}
+
+#endif
