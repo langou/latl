645ee6ab830e2cc3be3aeb81fec41d61594ee6f5
Rodney James <rodney.james@ucdenver.edu>
2013-05-01 10:45:36 -0600

    added TREVC

---
+++ include/trevc.h
@@ -0,0 +1,22 @@
+//
+//  trevc.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 5/1/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _trevc_h
+#define _trevc_h
+
+/// @file trevc.h Computes eigenvectors of a quasi-triangular matrix from a Schur factorization.
+
+namespace LATL
+{
+   template<typename real_t>
+   int TREVC()
+   {
+      
+   }
+}
+#endif
