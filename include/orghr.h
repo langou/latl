3b70c6d0bb67844a13dd8138e9ea5404bb05bb45
Rodney James <rodney.james@ucdenver.edu>
2013-05-01 16:33:11 -0600

    added ORGHR

---
+++ include/orghr.h
@@ -0,0 +1,25 @@
+//
+//  orghr.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 5/1/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _orghr_h
+#define _orghr_h
+
+/// @file orghr.h Generates a real orthogonal matrix from elementary reflectors.
+
+#include "latl.h"
+
+namespace LATL
+{
+   template<typename real_t>
+   int ORGHR(int_t n,real_t *A,int_t ldA,real_t *tau)
+   {
+
+   }
+}
+
+#endif
