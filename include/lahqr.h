9f6bc0c9dee3ed9769cd6e2672918febaf88bf78
Rodney James <rodney.james@ucdenver.edu>
2013-05-01 10:45:19 -0600

    added LAHQR

---
+++ include/lahqr.h
@@ -0,0 +1,23 @@
+//
+//  lahqr.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 5/1/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _lahqr_h
+#define _lahqr_h
+
+/// @file lahqr.h Computes eigenvalues and Schur factorization of upper Hessenberg matrix.
+
+namespace LATL
+{
+   template<typename real_t>
+   int LAHQR()
+   {
+
+   }
+}
+
+#endif
