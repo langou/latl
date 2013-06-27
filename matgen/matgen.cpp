07ca3a9999f903b814010119cfc7b883255b63b8
Rodney James <rodney.james@ucdenver.edu>
2013-06-26 20:11:51 -0600

    updated matgen to use matrix template functions

--- matgen/matgen.cpp
+++ matgen/matgen.cpp
@@ -1,13 +1,12 @@
 //
-//  matgen.cpp
+//  LATL.cpp
 //  Linear Algebra Template Library
 //
-//  Created by Rodney James on 1/24/13.
+//  Created by Rodney James on 4/3/13.
 //  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
 //
-//  [generates a matrix of specified size and prints to standard output]
-//
 
+#include <complex>
 #include <iostream>
 #include <fstream>
 #include <cstdlib>
@@ -15,8 +14,9 @@
 #include <limits>
 #include <cstring>
 #include <print.h>
-#include <latl.h>
-#include <larnv.h>
+#include <hilbert.h>
+#include <morgan.h>
+#include <grcar.h>
 
 typedef long double ldouble;
 
@@ -24,62 +24,95 @@ typedef long double ldouble;
 #define REAL double
 #endif
 
-typedef REAL real_t;
+typedef REAL Real;
+
+
+template<typename T> void output(int m,int n,T *A,char *outfile)
+{
+   using std::cerr;
+   using std::endl;
+   using LATL::Print;
+   
+   if(A)
+   {
+      if(outfile==NULL)
+      {
+         Print<T>(n,n,A,n);
+      }
+      else
+      {
+         std::ofstream out(outfile);
+         if(out)
+            Print<T>(m,n,A,n,out);
+         else
+         {
+            cerr << "Cannot open output file " << outfile << "." << endl;
+            exit(0);
+         }
+      }
+   }
+   else
+   {
+      cerr << "Cannot create matrix." << endl;
+      exit(0);
+   }
+}
 
 void usage(char *name)
 {
    using std::cerr;
    using std::endl;
    
-   cerr << "Usage: " << name << " [-o <file>] [-c] [-r <dist>] [-m <m>] [-n <n>] [-S <seed>] [-H] [-s] [-h] [-p]";
+   cerr << "Usage: " << name << " [-o <file>] [-m <m>] [-n <n>] [-k <k>] [-hilbert] [-morgan] [-grcar]";
    cerr << endl;
    cerr << "        -o <file> save matrix to <file>, otherwise writes matrix to standard output" << endl;
-   cerr << "        -c        generates complex matrix (default is real)" << endl;
    cerr << "        -m <m>    sets number of rows (default is m=1)" << endl;
    cerr << "        -n <n>    sets number of columns (default is n=1)" << endl;
-   cerr << "        -H        creates n-by-n Hilbert matrix" << endl;
-   cerr << "        -h        creates n-by-n hermitian matrix" << endl;
-   cerr << "        -s        creates n-by-n symmetric matrix" << endl;
-   cerr << "        -p        creates n-by-n positive definite matrix by adding n to the diagonal" << endl;
-   cerr << "        -S <seed> sets random number generator seed (default is 0)" << endl;
-   cerr << "        -r <dist> creates M x N random matrix with one of the following distributions: " << endl;
-   cerr << "                    1 = uniform on (0,1) (default)" << endl;
-   cerr << "                    2 = uniform on (-1,1)" << endl;
-   cerr << "                    3 = normal on (0,1)" << endl;
-   cerr << "                    4 = uniformly distributed on the disc abs(z) < 1 (complex)" << endl;
-   cerr << "                    5 = uniformly distributed on the circle abs(z) = 1 (complex)" << endl;
+   cerr << "        -hilbert  creates n-by-n Hilbert matrix" << endl;
+   cerr << "        -morgan   creates n-by-n Morgan matrix" << endl;
+   cerr << "        -grcar    creates n-by-n Grcar matrix with k superdiagonals" << endl;
    exit(0);
 }
+
 
 int main(int argc, char** argv)
 {
    using std::cout;
    using std::cerr;
    using std::endl;
-   using std::numeric_limits;
    using std::strncmp;
    using std::atoi;
-   using std::real;
-   using std::conj;
-   using std::exp;
-   using std::log;
-   using std::log2;
-   using LATL::LARNV;
+   
+   using LATL::Print;
+   using LATL::Hilbert;
+   using LATL::Morgan;
+   using LATL::Grcar;
+   
+   enum MatrixType {hilbert,morgan,grcar,none};
+
    int m=1;
    int n=1;
+   int k=1;
    int arg=1;
-   uint32_t s=0;
-   int dist=1;
-   bool use_complex=0;
-   bool symmetric=0;
-   bool hermitian=0;
-   bool hilbert=0;
-   bool positive=0;
    char *outfile=NULL;
+
+   MatrixType matrix=none;
    
    while(arg<argc)
    {
-      if(strncmp(argv[arg],"-m",2)==0)
+      if(strncmp(argv[arg],"-morgan",7)==0)
+      {
+         matrix=morgan;
+      }
+      else if(strncmp(argv[arg],"-hilbert",8)==0)
+      {
+         matrix=hilbert;
+      }
+      else if(strncmp(argv[arg],"-grcar",8)==0)
+      {
+         matrix=grcar;
+      }
+      else if(strncmp(argv[arg],"-m",2)==0)
       {
          arg++;
          if(arg<argc)
@@ -95,41 +128,11 @@ int main(int argc, char** argv)
          if(n<1)
             usage(argv[0]);
       }
-      else if(strncmp(argv[arg],"-S",2)==0)
+      else if(strncmp(argv[arg],"-k",2)==0)
       {
          arg++;
          if(arg<argc)
-            s=atoi(argv[arg]);
-         if(s<0)
-            usage(argv[0]);
-      }
-      else if(strncmp(argv[arg],"-r",2)==0)
-      {
-         arg++;
-         if(arg<argc)
-            dist=atoi(argv[arg]);
-         if(dist<0)
-            usage(argv[0]);
-      }
-      else if(strncmp(argv[arg],"-c",2)==0)
-      {
-         use_complex=1;
-      }
-      else if(strncmp(argv[arg],"-s",2)==0)
-      {
-         symmetric=1;
-      }
-      else if(strncmp(argv[arg],"-h",2)==0)
-      {
-         hermitian=1;
-      }
-      else if(strncmp(argv[arg],"-H",2)==0)
-      {
-         hilbert=1;
-      }
-      else if(strncmp(argv[arg],"-p",2)==0)
-      {
-         positive=1;
+            k=atoi(argv[arg]);
       }
       else if(strncmp(argv[arg],"-o",2)==0)
       {
@@ -144,115 +147,24 @@ int main(int argc, char** argv)
       arg++;
    }
    
-   if(hilbert)
+   if(matrix==hilbert)
    {
-      hermitian=0;
-      symmetric=1;
-      use_complex=0;
-      dist=0;
+      Real *A=Hilbert<Real>(n);
+      output<Real>(n,n,A,outfile);
    }
-
-   if(positive)
+   else if(matrix==morgan)
    {
-      if(use_complex)
-         hermitian=1;
-      else
-         symmetric=1;
+      Real *A=Morgan<Real>(n,k);
+      output<Real>(n,n,A,outfile);
    }
-
-   if(hermitian||symmetric)
-      m=n;
-   
-   if(use_complex)
+   else if(matrix==grcar)
    {
-      if(hermitian&&symmetric)
-         symmetric=0;
-      complex<real_t> *A=new complex<real_t>[m*n];
-      if((dist>0)&&(dist<6))
-      {
-         LARNV(dist,m*n,A,s);
-         if(symmetric||hermitian)
-         {
-            for(int i=0;i<n;i++)
-            {
-               A[i+i*n]=real(A[i+i*n]);
-               if(symmetric)
-                  for(int j=i+1;j<n;j++)
-                     A[i+j*n]=A[j+i*n];
-               else
-                  for(int j=i+1;j<n;j++)
-                     A[i+j*n]=conj(A[j+i*n]);
-            }
-         }
-         if(positive)
-         {
-            for(int i=0;i<n;i++)
-               A[i+n*i]+=(real_t)n;
-         }
-      }
-      if(outfile==NULL)
-      {
-         LATL::Print<real_t>(m,n,A,m);
-      }
-      else
-      {
-         std::ofstream out(outfile);
-         if(out)
-            LATL::Print<real_t>(m,n,A,m,out);
-         else
-         {
-            cerr << "Cannot open output file." << endl;
-            return 1;
-         }
-      }
+      Real *A=Grcar<Real>(n,k);
+      output<Real>(n,n,A,outfile);
    }
    else
    {
-      real_t *A=new real_t[m*n];
-      if(hermitian||symmetric)
-      {
-         hermitian=0;
-         symmetric=1;
-      }
-      if(hilbert)
-      {
-         for(int j=0;j<n;j++)
-            for(int i=0;i<n;i++)
-               A[i+j*n]=1.0/(real_t)(i+j+1);
-      }
-      else if((dist>0)&&(dist<4))
-      {
-         LARNV(dist,m*n,A,s);
-         if(symmetric)
-         {
-            for(int i=0;i<n;i++)
-            {
-               A[i+i*n]=real(A[i+i*n]);
-               for(int j=i+1;j<n;j++)
-                  A[i+j*n]=A[j+i*n];
-            }
-         }
-         if(positive)
-         {
-            for(int i=0;i<n;i++)
-               A[i+n*i]+=(real_t)n;
-         }
-      }
-      if(outfile==NULL)
-      {
-         LATL::Print<real_t>(m,n,A,m);
-      }
-      else
-      {
-         std::ofstream out(outfile);
-         if(out)
-            LATL::Print<real_t>(m,n,A,m,out);
-         else
-         {
-            cerr << "Cannot open output file." << endl;
-            return 1;
-         }
-      }
+      usage(argv[0]);
    }
    return 0;
 }
