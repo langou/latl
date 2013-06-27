1f4c2fe9a3c87f54c64f103a12184b494ac448f1
Rodney James <rodney.james@ucdenver.edu>
2013-06-26 20:01:13 -0600

    added Grcar matrix generator

---
+++ include/grcar.h
@@ -0,0 +1,67 @@
+//
+//  grcar.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 4/3/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _grcar_h
+#define _grcar_h
+
+/// @file grcar.h Generates grcar matrix.
+
+#include <cstdlib>
+#include <limits>
+#include <cmath>
+
+namespace LATL
+{
+   /// @brief Generates grcar matrix of order n and k superdiagonals.
+   /// @details The grcar matrix is a n-by-n Toeplitz matrix with A with
+   ///
+   ///           A(i,i)   =  1 for i=1,...,n
+   ///           A(i,i-1) = -1 for i=2,...,n
+   ///           A(i,i+1) =  1 for i=1,...,n-1
+   ///                    .
+   ///                    .
+   ///                    .
+   ///           A(i,i+k) =  1 for i=1,...,n-k
+   ///           A(i,j)   =  0 otherwise
+   /// Computing eigenvalues of this matrix is very sensitive to the numerical precision used.
+   /// Reference: <A HREF="http://math.nist.gov/MatrixMarket/data/NEP/mvmgrc/mvmgrc.html">J. Grcar</A>.
+   /// @tparam T Type of matrix elements.
+   /// @return Pointer to matrix of type T containing n-by-n grcar matrix.
+   /// @return NULL if a parameter is invalid.
+   /// @param n Order of matrix.  n>0.
+   /// @param k Number of superdiagonals. k>0.
+   
+   template<typename T> T *Grcar(int n,int k)
+   {
+      const T one(1.0);
+      const T zero(0.0);
+      
+      if((n<1)||(k<0))
+         return NULL;
+
+      T *M=new T[n*n];
+      if(M)
+      {
+         for(int i=0;i<n;i++)
+            for(int j=0;j<n;j++)
+               M[i+j*n]=zero;
+
+         for(int i=0;i<n;i++)
+            M[i+i*n]=one;
+         
+         for(int i=1;i<n;i++)
+            M[i+(i-1)*n]=-one;
+
+         for(int j=1;j<=k;j++)
+            for(int i=0;i<n-j;i++)
+               M[i+(i+j)*n]=one;
+      }
+      return M;
+   }
+}
+#endif
