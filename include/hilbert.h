6a63735e4849ba946c3be72bec77b5f5028491ad
Rodney James <rodney.james@ucdenver.edu>
2013-06-26 20:01:26 -0600

    added Hilbert matrix generator

---
+++ include/hilbert.h
@@ -0,0 +1,43 @@
+//
+//  hilbert.h
+//  Linear Algebra Template Library
+//
+//  Created by Rodney James on 4/3/13.
+//  Copyright (c) 2013 University of Colorado Denver. All rights reserved.
+//
+
+#ifndef _hilbert_h
+#define _hilbert_h
+
+/// @file hilbert.h Generates Hilbert matrix.
+
+#include <cstdlib>
+
+namespace LATL
+{
+   /// @brief Generates Hilbert matrix of order n.
+   /// @details The Hilbert matrix H is defined as
+   ///
+   ///           H(i,j) = 1/(i+j-1); i,j=1,...,n.
+   /// @tparam T Type of matrix elements.
+   /// @return Pointer to matrix of type T containing n-by-n Hilbert matrix.
+   /// @return NULL if parameter invalid.
+   /// @param n Order of matrix.  n>0.
+   /// @ingroup matgen
+
+   template<typename T> T *Hilbert(int n)
+   {
+      if(n<1)
+         return NULL;
+
+      T *M=new T[n*n];
+      if(M)
+      {
+         for(int i=0;i<n;i++)
+            for(int j=0;j<=i;j++)
+               M[i+j*n]=M[j+i*n]=static_cast<T>(1)/static_cast<T>(i+j+1);
+      }
+      return M;
+   }
+}
+#endif
