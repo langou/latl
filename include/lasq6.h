2575083e87c991d493fbb1d030b2784e58c691e4
Philippe Theveny <philippe.theveny@ucdenver.edu>
2015-05-04 11:52:21 -0600

    fixed bugs

--- include/lasq6.h
+++ include/lasq6.h
@@ -39,8 +39,8 @@ namespace LATL
   /// @ingroup AUX
   template<typename real_t>
   void LASQ6(const int_t i0, const int_t n0, real_t *z, const int_t pp,
-						 real_t &dmin, real_t &dmin1, real_t &dmin2,
-						 real_t &dn, real_t &dnm1, real_t &dnm2)
+             real_t &dmin, real_t &dmin1, real_t &dmin2,
+             real_t &dn, real_t &dnm1, real_t &dnm2)
   {
     using std::min;
 
@@ -52,9 +52,8 @@ namespace LATL
 
     real_t emin;
     real_t d;
-    real_t dmin;
     real_t temp;
-    
+
     emin=z[4*i0+pp]; // why emin=q(i0+1) or qq(i0+1)?
     d=z[4*(i0-1)+pp];
     dmin=d;
@@ -62,71 +61,71 @@ namespace LATL
     if (pp==0)
     {
       // ping: z[4xj+1] and z[4xj+3] are updated using z[4xj] and z[4xj+2]
-      for (j4=4*(i0-1); j4<4*(n0-3); j4+=4)
+      for (int_t j4=4*(i0-1); j4<4*(n0-3); j4+=4)
       {
-				z[j4+1]=d+z[j4+2];
-				if (z[j4+1]==zero)
-				{
-					// updated q(j) is zero
-					z[j4+3]=zero;
-					d=z[j4+4];
-					dmin=d;
-					emin=zero;
-				}
-				else if ((safemin*z[j4+4]<z[j4+1]) && (safemin*z[j4+1]<z[j4+4]))
-				{
-					// no possible under-/overflow: factorize quotient
-					temp=z[j4+4]/z[j4+1];
-					z[j4+3]=z[j4+2]*temp;
-					d=d*temp;
-				}
-				else
-				{
-					// avoid under-/overflow
-					z[j4+3]=z[j4+4]*(z[j4+2]/z[j4+1]);
-					d=z[j4+4]*(d/z[j4+1]);
-				}
-				dmin=min(dmin,d);
-				emin=min(emin,z[j4+3]);
+        z[j4+1]=d+z[j4+2];
+        if (z[j4+1]==zero)
+        {
+          // updated q(j) is zero
+          z[j4+3]=zero;
+          d=z[j4+4];
+          dmin=d;
+          emin=zero;
+        }
+        else if ((safemin*z[j4+4]<z[j4+1]) && (safemin*z[j4+1]<z[j4+4]))
+        {
+          // no possible under-/overflow: factorize quotient
+          temp=z[j4+4]/z[j4+1];
+          z[j4+3]=z[j4+2]*temp;
+          d=d*temp;
+        }
+        else
+        {
+          // avoid under-/overflow
+          z[j4+3]=z[j4+4]*(z[j4+2]/z[j4+1]);
+          d=z[j4+4]*(d/z[j4+1]);
+        }
+        dmin=min(dmin,d);
+        emin=min(emin,z[j4+3]);
       }
     }
     else
     {
       // pong: z[4xj] and z[4xj+2] are updated using z[4xj+1] and z[4xj+3]
-      for (j4=4*(i0-1); j4<4*(n0-3); j4+=4)
+      for (int_t j4=4*(i0-1); j4<4*(n0-3); j4+=4)
       {
-				z[j4]=d+z[j4+3];
-				if (z[j4]==zero)
-				{
-					// updated q(j) is zero
-					z[j4+2]=zero;
-					d=z[j4+5];
-					dmin=d;
-					emin=zero;
-				}
-				else if ((safemin*z[j4+5]<z[j4]) && (safemin*z[j4]<z[j4+5]))
-				{
-					// no possible under-/overflow: factorize quotient
-					temp=z[j4+5]/z[j4];
-					z[j4+2]=z[j4+3]*temp;
-					d=d*temp;
-				}
-				else
-				{
-					// avoid under-/overflow
-					z[j4+2]=z[j4+5]*(z[j4+3]/z[j4]);
-					d=z[j4+5]*(d/z[j4]);
-				}
-				dmin=min(dmin,d);
-				emin=min(emin,z[j4+2]);
+        z[j4]=d+z[j4+3];
+        if (z[j4]==zero)
+        {
+          // updated q(j) is zero
+          z[j4+2]=zero;
+          d=z[j4+5];
+          dmin=d;
+          emin=zero;
+        }
+        else if ((safemin*z[j4+5]<z[j4]) && (safemin*z[j4]<z[j4+5]))
+        {
+          // no possible under-/overflow: factorize quotient
+          temp=z[j4+5]/z[j4];
+          z[j4+2]=z[j4+3]*temp;
+          d=d*temp;
+        }
+        else
+        {
+          // avoid under-/overflow
+          z[j4+2]=z[j4+5]*(z[j4+3]/z[j4]);
+          d=z[j4+5]*(d/z[j4]);
+        }
+        dmin=min(dmin,d);
+        emin=min(emin,z[j4+2]);
       }
     }
 
     // last two steps are unrolled so as to record last d values
     dnm2=d;
     dmin2=dmin;
-    j4=4*(n0-3)+pp; // index of updated values
-    j4old=4*(n0-3)-pp+1; // index of old values
+    int_t j4=4*(n0-3)+pp; // index of updated values
+    int_t j4old=4*(n0-3)-pp+1;  // index of old values
     z[j4]=dnm2+z[j4old+2];
     if(z[j4]==zero)
     {
@@ -146,7 +145,7 @@ namespace LATL
     else
     {
       // avoid under-/overflow
-      z[j4+2]=z[j4old+4]*(z[j4old+2)/z[j4]);
+      z[j4+2]=z[j4old+4]*(z[j4old+2]/z[j4]);
       dnm1=z[j4old+4]*(dnm2/z[j4]);
     }
     dmin=min(dmin,dmin1);
@@ -173,7 +172,7 @@ namespace LATL
     else
     {
       // avoid under-/overflow
-      z[j4+2]=z[j4old+4]*(z[j4old+2)/z[j4]);
+      z[j4+2]=z[j4old+4]*(z[j4old+2]/z[j4]);
       dn=z[j4old+4]*(dnm1/z[j4]);
     }
     dmin=min(dmin,dn);
